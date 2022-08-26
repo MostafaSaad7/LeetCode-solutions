61c6332d815f48bce407c5a180e790be97ce1b78
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-08-26 16:44:15 +0200

    Time: 28 ms (64.66%), Space: 17.9 MB (64.62%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,13 @@
+//from discuss
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+		int total = n*(n+1) / 2;                     
+        
+        for(int i=0; i<nums.size(); i++) {
+            total -= nums[i];
+        }
+        return total;
+    }
+};
