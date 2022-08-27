0ccbdc18d39067a0e132af9f675c40ae1643ee1c
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-08-27 23:04:55 +0200

    Time: 14 ms (98.26%), Space: 17 MB (53.51%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    // just XOR elements with each other 
-    //same elements cancel each other 
+
     int singleNumber(vector<int>& nums) {
         int res = 0;
         for(int num : nums)
