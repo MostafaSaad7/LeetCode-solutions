2ba25f469db5d575a327ea3e624b6f6b3feacaf3
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-06 10:37:44 +0200

    Time: 133 ms (64.74%), Space: 116.9 MB (6.60%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
+        unordered_set<int> s(1e5+2);
         for(int i : nums) {
             if(s.insert(i).second==0) return true;
         }
