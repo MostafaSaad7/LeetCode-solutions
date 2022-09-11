58da77d161506d31a01c280f0849e83edd74f11d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-11 08:39:26 +0200

    Time: 11 ms (37.97%), Space: 13.9 MB (18.29%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,43 +1,64 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return dfs(preorder,0,preorder.size()-1);
-    }
-    
-    TreeNode * dfs(vector<int>& preorder , int left , int right)
-    {
-        if(left > right )
-            return nullptr;
-        TreeNode * root = new TreeNode(preorder[left]);
-        for(int  i = left+1 ; i<= right ; i++)
-        {
-            if(preorder[i]> preorder[left])
-        
-            { 
-            root->left = dfs(preorder , left +1, i-1);
-            root->right = dfs(preorder , i,right );
-                break;
-            }
-            else if(i == right)
-            {
-                root->left = dfs(preorder , left +1, right);
+// #include <iostream>
+// #include <cassert>
+// #include <vector>
+// #include <queue>
+// #include <stack>
+// #include <algorithm>
+// using namespace std;
 
-            }
-        }
-        
-        return root ;
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution
+{
+public:
+    TreeNode *bstFromPreorder(vector<int> &preorder)
+    {
+        vector<int> nextGreaterIndx = next_greater_idx(preorder);
+        return dfs(preorder, 0, preorder.size() - 1, nextGreaterIndx);
     }
-        
-        
-    };
+
+    TreeNode *dfs(vector<int> &preorder, int left, int right, vector<int> &nextGreaterIndx)
+    {
+        if (left > right)
+            return nullptr;
+        TreeNode *root = new TreeNode(preorder[left]);
+        root->left = dfs(preorder, left + 1, nextGreaterIndx[left] - 1, nextGreaterIndx);
+        root->right = dfs(preorder, nextGreaterIndx[left], right, nextGreaterIndx);
+
+        return root;
+    }
+
+    vector<int> next_greater_idx(vector<int> &preorder)
+    {
+        if (preorder.size() == 0)
+            return {};
+        vector<int> nextGreaterIndx(preorder.size());
+        stack<int> smallIndices;
+        smallIndices.push(0);
+        for (int i = 1; i < preorder.size(); i++)
+        {
+
+            while (!smallIndices.empty() && preorder[i] > preorder[smallIndices.top()])
+            {
+                nextGreaterIndx[smallIndices.top()] = i;
+                smallIndices.pop();
+            }
+            smallIndices.push(i);
+        }
+
+        while (!smallIndices.empty())
+        {
+            nextGreaterIndx[smallIndices.top()] = preorder.size();
+            smallIndices.pop();
+        }
+        return nextGreaterIndx;
+    }
+};
