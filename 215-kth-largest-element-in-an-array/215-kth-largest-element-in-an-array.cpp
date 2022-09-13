3995e621a40e39c1b1f13fbd08789aced1bdec36
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-13 18:02:59 +0200

    Time: 138 ms (77.43%), Space: 50 MB (11.56%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,123 +1,15 @@
 class Solution {
-    class MinHeap {
-private:
-   int capacity{};
-	int *array{};
-	int size{};
-        
-    int left(int node)
-	{
-		int p = 2 * node + 1;
-		if (p >= size)
-			return -1;
-		return p;
-	}
-        
-	int right(int node)
-	{
-		int p = 2 * node + 2;
-		return p >= size ? -1 : p;
-	}
-        
-	int parent(int node)
-	{
-		return node == 0 ? -1 : (node - 1) / 2;
-	
-    }
-        
-	void heapify_up(int child_pos)
-	{
-		// stop when parent is smaller (or no parent)
-		int par_pos = parent(child_pos);
-		if (child_pos == 0 || array[par_pos] < array[child_pos])
-			return;
-
-			swap(array[child_pos], array[par_pos]);
-			heapify_up(par_pos);
-	}
-
-	void heapify_down(int parent_pos)
-	{ // O(logn)
-		int child_pos = left(parent_pos);
-		int right_child = right(parent_pos);
-
-		if (child_pos == -1) // no children
-			return;
-		// is right smaller than left?
-		if (right_child != -1 && array[right_child] < array[child_pos])
-			child_pos = right_child;
-
-		if (array[parent_pos] > array[child_pos])
-		{
-			swap(array[parent_pos], array[child_pos]);
-			heapify_down(child_pos);
-		}
-	}
-  
-public: 
-        
-public:
-	MinHeap()
-	{
-		array = new int[capacity]{};
-		size = 0;
-	}
-
-
-	MinHeap(int k)
-	{
-        capacity = k;
-		array = new int[capacity]{};
-		size =0;
-	}
-	~MinHeap()
-	{
-		delete[] array;
-		array = nullptr;
-	}
-
-	int top()
-	{
-		assert(!isempty());
-		return array[0];
-	}
-
-	void push(int key)
-	{
-		assert(size + 1 <= capacity);
-		array[size++] = key;
-		heapify_up(size - 1);
-	}
-
-	void pop()
-	{
-		assert(!isempty());
-		array[0] = array[--size];
-		heapify_down(0);
-	}
-
-	bool isempty()
-	{
-		return size == 0;
-	}
-    
-    int HeapSize()
-    {
-          return size;
-    }
-    };
+ 
         
 public:
     int findKthLargest(vector<int>& nums, int k) {
-    MinHeap heap(k);
-        for(int i =0 ; i < (int)nums.size();i++)
-        if(heap.HeapSize()<k)
-            heap.push(nums[i]);
-        else if (heap.top() < nums[i]){
-            heap.pop();
-            heap.push(nums[i]);
-        }
-        return heap.top();
+        priority_queue<int> q;
+        for(int i = 0 ; i< nums.size();i++)
+            q.push(nums[i]);
+        while(--k)
+            q.pop();
+
+        return q.top();
     }
     
     
