13153c1f912148d6e4b22e7e27c0944dabf0953b
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-14 09:40:44 +0200

    Time: 426 ms (5.03%), Space: 45.5 MB (87.16%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,32 @@
 class Solution {
- 
         
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> q;
-        for(int i = 0 ; i< nums.size();i++)
-            q.push(nums[i]);
-        while(--k)
-            q.pop();
 
-        return q.top();
+        return quickSelection(nums , 0, nums.size()-1,nums.size()-k);
     }
     
+    int quickSelection(vector<int>& nums,int left , int right,int k)
+    {
+        int  piviot = nums [right];
+        int p = left;
+        for(int i =left ;  i<right;i++)
+        {
+            if(nums[i] <= piviot)
+            {
+                swap(nums[i],nums[p]);
+                p++; 
+            }
+        }
+        
+        swap(nums[p] ,nums[right]);
+        
+        if(k<p)
+            return quickSelection(nums,left,p-1,k);
+        else if(k>p)
+            return quickSelection(nums,p+1,right,k);
+        else
+            return nums[p];
+    }
     
 };
