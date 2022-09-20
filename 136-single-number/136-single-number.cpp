d744d28d0d2435d8f0a41d784f07158a28dc3be0
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-20 14:02:01 +0200

    Time: 16 ms (96.60%), Space: 17 MB (53.57%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,25 +3,10 @@ public:
 
     int singleNumber(vector<int>& nums) {
         int res = 0;
-        unordered_map<int,int >mp ;
-        
         for(int num : nums)
-        {
-            if(mp.count(num)==0)
-                mp[num]=1;
-            else
-            {
-               mp[num]++;
-
-            }    
-        }    
-        for(int num : nums)
-        {
-            if(mp[num]==1)
-                return num;
-            
-        } 
+            res^=num;
         return res;
-
+        
+ 
     }
 };
