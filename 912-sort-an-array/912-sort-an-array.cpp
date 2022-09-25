f26cf8b8f62db8f20bf1d041a13752c5ecee172c
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-25 15:08:58 +0200

    Time: 200 ms (44.55%), Space: 58.6 MB (30.14%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,44 +1,27 @@
+
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        int min =nums[0];
-        int maxN = nums[0];
-        
-        for (int i =1 ; i< nums.size(); i++)
-        {
-            if(nums[i] < min)
-                min = nums[i];         
-        }
-        
-        min =abs(min);
-        
-        for (int i =0 ; i< nums.size(); i++)
-        {
-            nums[i]+=min;
-            
-        }
-        for (int i =0 ; i< nums.size(); i++)
-        {
-            if(nums[i] > maxN)
-                maxN = nums[i];         
-        }
-        
-        vector<int > vec (maxN+1);
-        
-        for (int i =0 ; i< nums.size(); i++)
-        {
-            vec[nums[i]]+=1;
-        }
-        
-        int idx=0;
-        for(int i = 0 ; i <= maxN ; i++)
-        {
-            for(int j = 0 ; j < vec[i]; j++,idx++)
-            {
-                nums[idx]=i-min ;
-            }
-        }
-        
-        return nums;
-    }
+	vector<int> sortArray(vector<int> &array) {
+		int size = array.size();
+		int mxVal = array[0], mnVal = array[0];
+
+		for (int i = 1; i < size; ++i) {
+			if (array[i] > mxVal)
+				mxVal = array[i];
+			if (array[i] < mnVal)
+				mnVal = array[i];
+		}
+        int new_max = mxVal - mnVal;
+		vector<int> count(new_max+1);
+
+		for (int i = 0; i < size; ++i)
+			count[array[i] - mnVal] += 1;
+
+		int idx = 0;
+		for (int i = 0; i <= new_max; ++i) {
+			for (int j = 0; j < count[i]; ++j, ++idx)
+				array[idx] = i + mnVal;
+		}
+		return array;
+	}
 };
