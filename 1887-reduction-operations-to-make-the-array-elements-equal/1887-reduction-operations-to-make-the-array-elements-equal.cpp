13eb3bfd1fc466bfaa6575217dea8081d8bffafa
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-26 21:14:06 +0200

    Time: 553 ms (40.75%), Space: 82.9 MB (52.64%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 	int reductionOperations(vector<int> &nums) {
 		sort(nums.rbegin(), nums.rend());	
+
 		int operations = 0;
 		for (int i = 1; i < (int)nums.size(); ++i)
 			if (nums[i] != nums[i - 1])
