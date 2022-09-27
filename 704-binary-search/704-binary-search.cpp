b3d307471717ced766f81645086cb67217498ca1
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-27 10:53:19 +0200

    Time: 71 ms (42.06%), Space: 27.4 MB (92.07%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -7,7 +7,7 @@ public:
         int right = nums.size()-1;
         while(left<=right)
         {
-            int mid= (right+left)/2;
+            int mid= left+(right-left)/2;
             if(nums[mid] == target)
                 return mid;
             else if (target > nums[mid])
