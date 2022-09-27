5199acd2ced47aa555df138803fbcdbc74c9385e
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-27 14:17:47 +0200

    Time: 94 ms (83.43%), Space: 27.9 MB (63.67%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,21 +1,16 @@
-
 class Solution {
 public:
-	vector<int> findRightInterval(vector<vector<int>> &interevals) {
-		vector<vector<int>> startings;
-		for (int i = 0; i < (int) interevals.size(); ++i)
-			startings.push_back( { interevals[i][0], i });
-		sort(startings.begin(), startings.end());	// sort on unique starts
+	vector<int> findRightInterval(vector<vector<int>> &intervals) {
+		map<int, int> start_to_idx;
+		for (int i = 0; i < (int) intervals.size(); i++)
+			start_to_idx[intervals[i][0]] = i;
+		
+		vector<int> ans(intervals.size(), -1);
 
-		vector<int> ans(interevals.size(), -1);
-		for (int i = 0; i < (int) interevals.size(); ++i) {
-			vector<int> val { interevals[i][1], 0 };
-			auto iter = lower_bound(startings.begin(), startings.end(), val);
-
-			if (iter != startings.end()) {
-				int idx = iter - startings.begin();
-				ans[i] = startings[idx][1];
-			}
+		for (int i = 0; i < (int) intervals.size(); i++) {
+			auto iter = start_to_idx.lower_bound(intervals[i][1]);
+			if (iter != start_to_idx.end())
+				ans[i] = iter->second;
 		}
 		return ans;
 	}
