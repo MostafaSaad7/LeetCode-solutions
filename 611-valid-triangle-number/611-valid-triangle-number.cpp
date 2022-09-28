8ea9679c95fcfc28bbdba8052fe3584dcdf324b5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-28 14:11:58 +0200

    Time: 345 ms (31.27%), Space: 13 MB (53.55%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums) {
-       int count = 0, n = nums.size();
-		sort(nums.begin(), nums.end());
+ int triangleNumber(vector<int> &nums) {	// O(n^2) time
+        int count = 0, n = nums.size();
+        sort(nums.begin(), nums.end());
 
-		for (int i = 0; i < n - 2; i++) {
-			int k = i + 2;	// start after j
-			for (int j = i + 1; j < n - 1 && nums[i] != 0; j++) {
-				int first_idx = nums.size();
-				auto iter = lower_bound(nums.begin()+k, nums.end(), nums[i] + nums[j]);
-				if (iter != nums.end())
-					first_idx = iter - nums.begin();
-                count += first_idx - j - 1;
-			}
-		}
-		return count;   
+        for (int i = 0; i < n - 2; i++) {
+            int k = i + 2;	// observe k shared between js
+            // k: the first invalid index: c >= a+b
+            // We don't need to keep compute it for each j
+            // Last j found its k. We can that keep pushing it
+            // as anyway positions < k are valid
+            for (int j = i + 1; j < n - 1 && nums[i] != 0; j++) {
+                while (k < n && nums[i] + nums[j] > nums[k])
+                	++k;
+                count += k - j - 1;
+            }
+        }
+        return count;
     }
 };
