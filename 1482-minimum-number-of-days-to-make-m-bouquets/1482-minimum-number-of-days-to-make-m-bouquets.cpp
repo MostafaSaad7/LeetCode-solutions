0add3bf9c766b8cbef21d4781cecf8a1f0b0b350
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-29 16:49:39 +0200

    Time: 299 ms (52.32%), Space: 63.3 MB (24.71%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,50 +1,37 @@
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <iomanip>
-using namespace std;
-class Solution
-{
+//dr mostafa saad
+class Solution {
 public:
-    bool fun(vector<int> &bloomDay, int m, int k, int days)
-    {
-        int boq = 0;
-        int adj = 0;
-        for (int i = 0; i < (int)bloomDay.size(); ++i)
-        {
-            if (bloomDay[i] <= days)
-            {
-                ++adj;
+	int minDays(vector<int> &bloomDay, int total_bouquets, int flowers_per_bouquet) {
+		if (total_bouquets > (int) bloomDay.size() / flowers_per_bouquet)
+			return -1;	
 
-                if (adj >= k)
-                    adj = 0, ++boq;
+		int start = 1, end = *max_element(bloomDay.begin(), bloomDay.end()), min_days = -1;
 
-                if (boq == m) // done
-                    return true;
-            }
-            else
-                adj = 0;
-        }
-        return false;
-    }
-    int minDays(vector<int> &bloomDay, int m, int k)
-    {
-        if (m > (int)bloomDay.size()/k )
-            return -1;
-        int start = 1, end = *max_element(bloomDay.begin(), bloomDay.end());
-        int res = -1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (possible(bloomDay, total_bouquets, flowers_per_bouquet, mid))
+				end = mid - 1, min_days = mid;
+			else
+				start = mid + 1;
+		}
+		return min_days;
+	}
+	bool possible(vector<int> &bloomDay, int total_bouquets, int flowers_per_bouquet, int waiting_days) {
+		int adj_flowers = 0, bouquets = 0;
 
-        while (start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            if (fun(bloomDay, m, k, mid))
-                end = mid - 1, res = mid;
-            else
-                start = mid + 1;
-        }
-        return res;
-    }
+		for (int i = 0; i < (int) bloomDay.size(); ++i) {
+			if (bloomDay[i] <= waiting_days) {
+				++adj_flowers;
+
+				if (adj_flowers >= flowers_per_bouquet)
+					adj_flowers = 0, ++bouquets;	
+
+				if (bouquets == total_bouquets)	
+					return true;
+			} else
+				adj_flowers = 0;
+		}
+		return false;
+	}
 };
+
