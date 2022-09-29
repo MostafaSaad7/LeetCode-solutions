837252aa388d759d23a848448e88e36d05f304d0
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-09-29 18:57:50 +0200

    Time: 228 ms (13.16%), Space: 39.1 MB (5.58%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   bool possible(vector<int> &houses, vector<int> heaters, int ridus) {
+		int j = 0;
+		for (int i = 0; i < (int) heaters.size(); ++i) {
+			// Covered range [left, right] by the ith heater
+			int leftC = heaters[i] - ridus, rightC = heaters[i] + ridus;
+
+			while (j < (int) houses.size() && leftC <= houses[j] && houses[j] <= rightC)
+				++j;	// cover more houses with ith heater
+		}
+		return j == (int) houses.size();	// all is covered?
+	}
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end());
+		sort(heaters.begin(), heaters.end());
+
+		int start = 0, end = 1e9, radius = -1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (possible(houses, heaters, mid))	// find smallest
+				end = mid - 1, radius = mid;
+			else
+				start = mid + 1;
+		}
+		return radius;
+    }
+};
