6c39651f73f3f85656105037bbfce5979eb1a90e
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-04 08:28:59 +0200

    Time: 4 ms (58.65%), Space: 5.9 MB (24.74%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,19 +1,23 @@
 class Solution
 {
 public:
+    double fun(double val)
+    {
+        return val * val;
+    }
     int mySqrt(int x)
     {
-        double left = 0, right = x, mid=0;
-        while  (fabs(left- right)>1e-9)
+        double left = 0, right = x, mid = 0;
+        while (fabs(left - right) > 1e-9)
         {
-             mid = left + (right - left) / 2;
-            
-             if ((mid * mid) > x)
+            mid = left + (right - left) / 2;
+
+            if (fun(mid) > x)
                 right = mid;
             else
                 left = mid;
         }
 
-        return (int) (mid+1e-9);
+        return (int)(mid + 1e-9);
     }
 };
