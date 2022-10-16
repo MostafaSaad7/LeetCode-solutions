9f404f165d8d2daa8079f404747fafd0b6ec076e
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-16 14:57:03 +0200

    Time: 31 ms (28.41%), Space: 10 MB (24.62%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -22,7 +22,7 @@ public:
 		return false;
 	}
 
-	void dfs(int r, int c, vector<vector<int>> &grid,int ccid) {
+	void dfs(int r, int c, vector<vector<int>> &grid) {
 		if (!isvalid(r, c, grid) || grid[r][c]==-1 || grid[r][c] == 1)
 			return;		// invalid, visited or non-CC
 
@@ -31,20 +31,20 @@ public:
 			is_touched_boundry = true;
             return;
         }
-        grid[r][c]=-1;
+        grid[r][c]=-1; // mark as visited
 		for (int d = 0; d < 4; ++d)
-			dfs(r + dr[d], c + dc[d], grid, ccid);
+			dfs(r + dr[d], c + dc[d], grid);
 	}
 
 	int closedIsland(vector<vector<int>> &grid) {
 		vector<vector<int>> ccIDs(grid.size(), vector<int>(grid[0].size()));
-		int rows = grid.size(), cols = grid[0].size(), ccid = 0, count = 0;
+		int rows = grid.size(), cols = grid[0].size(), count = 0;
 
 		for (int r = 0; r < rows; ++r) {
 			for (int c = 0; c < cols; ++c) {
 				if (!grid[r][c]) {	// New CC component of ZEROs
 					is_touched_boundry = false;
-					dfs(r, c, grid, ++ccid);
+					dfs(r, c, grid);
 					count += !is_touched_boundry;
 				}
 			}
