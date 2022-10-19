04ffab4810279781603cb42cce5c4a6a18d43744
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-19 17:12:21 +0200

    Time: 360 ms (51.45%), Space: 22.6 MB (45.29%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,67 +1,58 @@
 class Solution
 {
-    int dr[4]{-1, 0, 1, 0};
-    int dc[4]{0, 1, 0, -1};
+private:
+	int dr[4]{-1, 0, 1, 0};
+	int dc[4]{0, 1, 0, -1};
+
+	bool isvalid(int r, int c, vector<vector<int>> &heights)
+	{
+		if (r < 0 || r >= (int)heights.size())
+			return false;
+		if (c < 0 || c >= (int)heights[0].size())
+			return false;
+		return true;
+	}
+
+	// We can use external flag. But let's code to return true
+	bool dfs(int r, int c, vector<vector<int>> &heights, vector<vector<bool>> &visited, int limit)
+	{
+
+		if (r == (int)heights.size() - 1 && c == (int)heights[0].size() - 1)
+			return true; // If reached the destination: base case for true
+		bool res = false;
+		visited[r][c] = 1;
+
+		for (int d = 0; d < 4; ++d)
+		{
+			int nr = r + dr[d], nc = c + dc[d];
+			if (!isvalid(nr, nc, heights) || visited[nr][nc] || abs(heights[r][c] - heights[nr][nc]) > limit)
+				continue;
+
+			res = dfs(nr, nc, heights, visited, limit) || res; // can we reach from this cell?
+		}
+		return res;
+	}
+
+	bool possible(vector<vector<int>> &heights, int mid)
+	{
+		vector<vector<bool>> visited(heights.size(), vector<bool>(heights[0].size()));
+		return dfs(0, 0, heights, visited, mid);
+	}
 
 public:
-    bool isvalid(int r, int c, vector<vector<int>> &heights)
-    {
-        if (r < 0 || r >= (int)heights.size())
-            return false;
-        if (c < 0 || c >= (int)heights[0].size())
-            return false;
-        return true;
-    }
+	int minimumEffortPath(vector<vector<int>> &heights)
+	{								  // O(MN logMax) time
+		int start = 0, end = 1000000; // The max height from the statement
+		int result = end;
 
-    int minimumEffortPath(vector<vector<int>> &heights)
-    {
-        int left = 0;
-        int right = 1000000;
-        int mid = 0;
-        int result = right;
-
-        // Binary search for steps
-        while (left <= right)
-        {
-            mid = left + (right - left) / 2;
-            if (ourFunction(heights, mid))
-            {
-                right = mid - 1;
-                result = mid;
-            }
-            else
-            {
-                left = mid + 1;
-            }
-        }
-
-        return result;
-    }
-
-    bool ourFunction(vector<vector<int>> &heights, int maxSteps)
-    {
-        vector<vector<bool>> visited(heights.size(), vector<bool>(heights[0].size()));
-        return DFS(heights, 0, 0, visited, maxSteps);
-    }
-
-    bool DFS(vector<vector<int>> &heights, int row, int col, vector<vector<bool>> &visited, int maxSteps)
-    {
-        if (row == heights.size() - 1 && col == (int)heights[0].size() - 1)
-            return true;
-        visited[row][col] = true;
-
-        for (int d = 0; d < 4; ++d) // check all neighbours
-        {
-            int newRow = row + dr[d];
-            int newCol = col + dc[d];
-
-            if (!isvalid(newRow, newCol, heights) || abs(heights[row][col] - heights[newRow][newCol]) > maxSteps || visited[newRow][newCol])
-                continue;
-
-            if (DFS(heights, newRow, newCol, visited, maxSteps)) // i need only one true so i can i use or ||
-                return true;
-        }
-
-        return false;
-    }
+		while (start <= end)
+		{
+			int mid = (start + end) / 2;
+			if (possible(heights, mid))
+				end = mid - 1, result = mid;
+			else
+				start = mid + 1;
+		}
+		return result;
+	}
 };
