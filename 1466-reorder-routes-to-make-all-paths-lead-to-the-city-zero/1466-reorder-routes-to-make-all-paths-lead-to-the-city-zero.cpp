a934fa5371967cf1f17ce2541b57badd22f93572
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-19 14:00:07 +0200

    Time: 856 ms (36.88%), Space: 116.2 MB (49.57%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -25,8 +25,7 @@ public:
 
     void DFS(GRAPH &graph, int cityToReach, vector<bool> &visited, int &result)
     {
-        if (visited[cityToReach])
-            return;
+    
         visited[cityToReach] = true;
         for (auto neighbours : graph[cityToReach])
         {
