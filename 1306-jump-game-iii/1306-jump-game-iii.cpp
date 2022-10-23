c3521fd435ced40dde4033f5d02ef45bfbc44bbf
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-23 14:32:49 +0200

    Time: 172 ms (7.30%), Space: 34.8 MB (46.92%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -9,6 +9,8 @@ public:
     }
     bool canReach(vector<int> &arr, int start)
     {
+        if(arr[start]==0)
+            return true;
         unordered_set<int> indicesSet;
         queue<int> indicesQ;
         indicesQ.push(start);
@@ -18,19 +20,21 @@ public:
         {
             int currentIndex = indicesQ.front();
             indicesQ.pop();
-            if (arr[currentIndex] == 0)
-                return true;
             int rightIndex = currentIndex + arr[currentIndex];
             int leftIndex = currentIndex - arr[currentIndex];
 
             if (isValid(rightIndex, sz) && indicesSet.count(rightIndex) == 0)
             {
+                if(arr[rightIndex]==0)
+                    return true;
                 indicesQ.push(rightIndex);
                 indicesSet.insert(rightIndex);
             }
 
             if (isValid(leftIndex, sz) && indicesSet.count(leftIndex) == 0)
             {
+                if(arr[leftIndex]==0)
+                    return true;
                 indicesQ.push(leftIndex);
                 indicesSet.insert(leftIndex);
             }
