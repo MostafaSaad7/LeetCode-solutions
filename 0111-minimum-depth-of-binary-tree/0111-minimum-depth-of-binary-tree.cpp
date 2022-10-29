3c8f9a721e57ae657bfe47b88201a3ecb1da2379
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-29 14:47:14 +0200

    Time: 636 ms (5.31%), Space: 144.7 MB (45.05%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -9,18 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int minDepth(TreeNode* root) {
-            if(root==nullptr)
-                return 0;
-           if(root->left==nullptr && root->right==nullptr)
-                return 1;
-        if(!root->left)
-            return 1+ minDepth(root->right);
-         if(!root->right)
-            return 1+ minDepth(root->left);
-        
-        return 1+min(minDepth(root->left),minDepth (root->right));
+    int minDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        for (int level = 0, sz = q.size(); !q.empty(); level++, sz = q.size())
+        {
+            while (sz--)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
+                if (!temp->left && !temp->right)
+                    return level + 1;
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
+        }
+        return 0;
     }
 };
