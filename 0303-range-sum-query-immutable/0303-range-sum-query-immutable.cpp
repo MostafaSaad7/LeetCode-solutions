79d6c180abce4f072ef9a7d60d57cd9dc212caaa
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-30 09:44:19 +0200

    Time: 64 ms (35.89%), Space: 17.1 MB (72.29%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,19 @@
+class NumArray
+{
+    vector<int> prefix;
+
+public:
+    NumArray(vector<int> &nums):  prefix(nums.size() + 1, 0)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        
+    }
+
+    int sumRange(int left, int right)
+    {
+        return prefix[right+1] - prefix[left];
+    }
+};
