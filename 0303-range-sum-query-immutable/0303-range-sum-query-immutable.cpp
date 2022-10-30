2d8d325e7e7e764947a48c1890c77fa6183bdee8
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-30 09:44:59 +0200

    Time: 69 ms (30.51%), Space: 17 MB (72.29%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,19 +1,14 @@
-class NumArray
-{
-    vector<int> prefix;
-
+class NumArray { // 12 ms, faster than 99.87%
 public:
-    NumArray(vector<int> &nums):  prefix(nums.size() + 1, 0)
-    {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            prefix[i + 1] = prefix[i] + nums[i];
-        }
-        
+    vector<int>& preSum; // `preSum` will reference to `nums` array, no copy at all!
+    
+    NumArray(vector<int>& nums) : preSum(nums) {
+        for (int i = 1; i < preSum.size(); ++i)
+            preSum[i] += preSum[i-1]; 
     }
-
-    int sumRange(int left, int right)
-    {
-        return prefix[right+1] - prefix[left];
+    
+    int sumRange(int left, int right) {
+        if (left == 0) return preSum[right];
+        return preSum[right] - preSum[left-1];
     }
 };
