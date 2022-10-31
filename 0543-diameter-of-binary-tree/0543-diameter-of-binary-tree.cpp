67da2e392e6d90f997a30f2d93bcb45bfd6928b5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-10-31 09:02:28 +0200

    Time: 12 ms (84.06%), Space: 20.3 MB (24.88%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,37 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int diameterOfBinaryTree(TreeNode* root){
-        return diameterOfBinaryTree2(root).first;
-    }
-    pair<int,int> diameterOfBinaryTree2(TreeNode* root) {
-        pair <int ,int>res= make_pair(0,0);
-        
-        if(!root->left && !root->right)
-                return res;
-        pair <int ,int>leftD= make_pair(0,0);
-        pair <int ,int>rightD= make_pair(0,0);
-        
-        if(root->left)
-            leftD=diameterOfBinaryTree2(root->left),res.first+= 1+leftD.second;
-        if(root->right)
-            rightD=diameterOfBinaryTree2(root->right),res.first+= 1+rightD.second;
-        
-        res.second = 1+max(leftD.second,rightD.second);
-        res.first = max(res.first,max(leftD.first,rightD.first));
-        
-        return res;
-        
+class Solution
+{
+    int maxD = 0;
 
+public:
+    int DFS(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+
+        int leftH = DFS(root->left);
+        int rightH = DFS(root->right);
+        maxD = max(maxD,leftH + rightH);
+        return 1 + max(leftH, rightH);
+    }
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        DFS(root);
+        return maxD;
     }
 };
