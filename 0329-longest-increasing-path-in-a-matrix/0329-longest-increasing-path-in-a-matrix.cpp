110ccf4d1654e0924767711ee534cf0503e8b75a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-06 13:10:01 +0200

    Time: 56 ms (86.76%), Space: 16 MB (51.02%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,75 +1,50 @@
 class Solution
 {
+public:
     int dr[4]{-1, 0, 1, 0};
     int dc[4]{0, 1, 0, -1};
-    bool isValid(int row, int col, int rows, int cols)
+    bool isvalid(int r, int c, vector<vector<int>> &matrix)
     {
-        if (row < 0 || row >= rows || col < 0 || col >= cols)
+        if (r < 0 || r >= (int)matrix.size())
+            return false;
+        if (c < 0 || c >= (int)matrix[0].size())
             return false;
         return true;
     }
-    int getInDeg(vector<vector<int>> &matrix, int row, int col)
+    int dfs(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &cache)
     {
-        int inDeg = 0;
-        for (int d = 0; d < 4; d++)
-        {
-            int r = row + dr[d], c = col + dc[d];
-            if (isValid(r, c, matrix.size(), matrix[0].size()) && matrix[row][col] > matrix[r][c])
-                inDeg++;
-        }
-        return inDeg;
-    }
-    vector<pair<int, int>> getNegibour(int row, int col, vector<vector<int>> &matrix)
-    {
-        vector<pair<int, int>> v;
-        for (int d = 0; d < 4; d++)
-        {
-            int r = row + dr[d], c = col + dc[d];
-            if (isValid(r, c, matrix.size(), matrix[0].size()) && matrix[r][c] > matrix[row][col])
-                v.push_back({r, c});
-        }
-        return v;
-    };
 
-public:
+        if (cache[i][j])
+            return cache[i][j];
+        for (int d = 0; d < 4; ++d)
+        {
+            int nr = i + dr[d], nc = j + dc[d];
+            if (!isvalid(nr, nc, matrix))
+                continue;
+            if (matrix[i][j] < matrix[nr][nc])
+                cache[i][j] = max(dfs(nr, nc, matrix, cache), cache[i][j]);
+        }
+
+        cache[i][j]++;
+        return cache[i][j];
+    }
     int longestIncreasingPath(vector<vector<int>> &matrix)
     {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<vector<int>> indegree(rows, vector<int>(cols));
-        queue<pair<int, int>> ready;
+        vector<vector<int>> cache(rows, vector<int>(cols, 0));
 
-        for (int r = 0; r < rows; ++r)
+        int res = 0;
+        for (int i = 0; i < rows; i++)
         {
-            for (int c = 0; c < cols; ++c)
+            for (int j = 0; j < cols; j++)
             {
-                indegree[r][c] = getInDeg(matrix, r, c);
-
-                if (indegree[r][c] == 0)
-                    ready.push({r, c});
+                res = max(res, dfs(i, j, matrix, cache));
             }
-        }
-        int level = 0;
-        while (!ready.empty())
-        {
-            int level_sz = ready.size();
-            while (level_sz--)
-            {
 
-                int row = ready.front().first;
-                int col = ready.front().second;
-                ready.pop();
-                vector<pair<int, int>> neighbours = getNegibour(row, col, matrix);
-                for (auto n : neighbours)
-                {
-                    if (--indegree[n.first][n.second] == 0)
-                    {
-                        ready.push({n.first, n.second});
-                    }
-                }
-            }
-            level++;
+            /* code */
         }
-        return level;
+
+        return res;
     }
 };
