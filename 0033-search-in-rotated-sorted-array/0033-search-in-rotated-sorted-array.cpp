17b3078ea5c0a8f6314fa1707754db3a4ef89982
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-07 23:35:37 +0200

    Time: 10 ms (36.97%), Space: 11.2 MB (28.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int> &nums, int target)
-    {
-        int left = 0;
-        int right = nums.size() - 1;
-        int mid = 0;
-        while (left <= right)
-        {
-            mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[left] <= nums[mid]) //left portion is sorted 
-            {
-                if (target < nums[mid] && target >= nums[left])
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            }
-            else
-            {
-                if (target > nums[mid] && target <= nums[right])
-                    left = mid + 1;
-                else
-                    right = mid - 1;
-            }
+    int search(vector<int > &A , int target) {
+        int n = A.size();
+        int lo=0,hi=n-1;
+        // find the index of the smallest value using binary search.
+        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(A[mid]>A[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=0;hi=n-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%n;
+            if(A[realmid]==target)return realmid;
+            if(A[realmid]<target)lo=mid+1;
+            else hi=mid-1;
         }
         return -1;
     }
