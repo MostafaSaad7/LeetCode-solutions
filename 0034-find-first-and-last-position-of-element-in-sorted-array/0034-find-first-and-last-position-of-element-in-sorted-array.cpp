b48f5787a20d93cce662045bdd9b4b2c5eb2363d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-15 13:02:54 +0200

    Time: 3 ms (99.61%), Space: 13.5 MB (98.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,63 +1,16 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-     
-        vector<int> v (2,-1);
-        int low = lower( nums, target);
-        if(low == -1)
+    vector<int> v (2,-1);
+        auto p = equal_range(nums.begin(),nums.end(),target);
+        if(p.first ==nums.end())
             return v;
-        int upp = upper( nums, target);
-            
-        v[0]=low;
-        if(upp==-1)
-            upp=nums.size();
-        v[1]=upp-1;
-        return v;
-        
-    }
-    
-    int lower(vector<int>& nums, int target) {
-        int res =-1;
-       if(nums.size()==0||target < nums[0] || target > nums[nums.size()-1])
-            return res;
-        int left = 0 ; 
-        int right = nums.size()-1;
-        
-        while(left<=right)
+        if(nums[p.first - nums.begin()] == target )
         {
-            int mid= left+(right-left)/2;
-            if(nums[mid] == target)
-            {
-                 res = mid;
-                right = mid -1 ;            
-            }
-            else if (target < nums[mid])
-                    right = mid -1 ;
-            else
-                left=mid+1;
+            v[0]=p.first - nums.begin();
+            v[1]=p.second-nums.begin()-1;
         }
+            return v;
         
-        return res;
     }
-    int upper(vector<int>& nums, int target) {
-        int left = 0 ; 
-        int res =-1;
-        int right = nums.size()-1;
-        
-        while(left<=right)
-        {
-            int mid= left+(right-left)/2;
-            if(nums[mid] > target)
-            {
-                res = mid;
-                right = mid-1;
-            }
-            else
-                left=mid+1;
-            
-        }
-        
-        return res;
-    }
-
 };
