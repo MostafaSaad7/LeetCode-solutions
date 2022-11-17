5146043ecd034917a963bb7b1fa01c71413c20fe
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-17 10:46:24 +0200

    Time: 4 ms (77.58%), Space: 5.8 MB (71.63%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	bool possible(int n, long long rows) {
+		long sum = (rows * (rows + 1)) / 2;
+		return n >= sum;
+	}
+	int arrangeCoins(int n) {
+		int left = 0, right = n, answer = 0;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (possible(n, mid))
+				left = mid + 1, answer = mid;
+			else
+				right = mid - 1;
+		}
+		return answer;
+	}
+};
