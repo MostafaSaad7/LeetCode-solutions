9ac2c93ed9dd6083fc32b7bbd0c8f97b26f63b51
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-19 15:10:16 +0200

    Time: 192 ms (82.63%), Space: 25.4 MB (47.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,48 +1,36 @@
+vector<int> g;
+const int MAX = 200 + 1;
+int memory[MAX][(200 * 100 + 1)];
+class Solution
+{
 
-const int MAX_N = 200;
-const int MAX_T = 100*200 + 1;
-int memory[MAX_N][MAX_T];
+    bool dp(int idx, int remaining)
+    {
+        if (remaining == 0)
+            return true;
+        if (idx == g.size() || remaining < 0)
+            return false;
+        int &ret = memory[idx][remaining];
+        if (ret != -1)
+            return ret;
+        if (dp(idx + 1, remaining - g[idx]))
+            return ret = true;
 
-vector<int> numbers;
+        return ret = dp(idx + 1, remaining);
+    }
 
-bool can_subset_sum(int idx, int target) {
-	// Common habit: cancel first invalid states
-	if (target < 0)
-		return false;
-
-	if (target == 0)
-		return true;	// Critical to handle first
-
-	if (idx == (int) numbers.size())
-		return 0;		// can't find combination
-
-	auto &ret = memory[idx][target];
-	if (ret != -1)
-		return ret;
-
-	if (can_subset_sum(idx + 1, target))
-		return ret = true;	// if leaving do it: perfect
-
-	return ret = can_subset_sum(idx + 1, target - numbers[idx]);
-	// we can check before subtracting but nicer way is to check
-	// early in the function. Easier for complex coding
-}
-
-bool subset_sum(const vector<int> &values, int target) {
-	numbers = values;
-	memset(memory, -1, sizeof(memory));
-	return can_subset_sum(0, target);
-}
-
-class Solution {
 public:
-	bool canPartition(vector<int> &nums) {
-		int sum_of_elems = accumulate(nums.begin(), nums.end(), 0);
-
-		if (sum_of_elems % 2 != 0)
-			return false;	// logically, numbers sum is even
-
-		return subset_sum(nums, sum_of_elems / 2);
-	}
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i : nums)
+        {
+            sum += i;
+        }
+        if (sum % 2 != 0)
+            return false;
+        g = nums;
+        memset(memory, -1, sizeof(memory));
+        return dp(0, sum / 2);
+    }
 };
-
