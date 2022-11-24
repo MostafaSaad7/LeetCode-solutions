c7d3d23a8bc8c24e42dd115065de202552cea357
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-24 11:44:04 +0200

    Time: 58 ms (22.84%), Space: 7.3 MB (85.40%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,37 @@
+const int MAX = 500 + 5;
+int memory[MAX][MAX];
+string w1;
+string w2;
+class Solution
+{
+public:
+    int dp(int index1 = 0, int index2 = 0)
+    {
+        if (index1 == w1.size())
+        {
+            return w2.size() - index2;
+        }
+        if (index2 == w2.size())
+        {
+            return w1.size() - index1;
+        }
+        int &ret = memory[index1][index2];
+        if (ret != -1)
+            return ret;
+
+        if (w1[index1] == w2[index2])
+            return ret = dp(index1 + 1, index2 + 1);
+        int del = 1 + dp(index1 + 1, index2);
+        int change = 1 + dp(index1 + 1, index2 + 1);
+        int insert = 1 + dp(index1, index2 + 1);
+
+        return ret = min({del, change, insert});
+    }
+    int minDistance(string word1, string word2)
+    {
+        w1 = word1;
+        w2 = word2;
+        memset(memory, -1, sizeof(memory));
+        return dp(0, 0);
+    }
+};
