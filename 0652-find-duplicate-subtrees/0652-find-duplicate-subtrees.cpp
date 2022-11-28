e8ded15b5ed36782e9693280f4f8b1e36184f1e5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-28 11:11:31 +0200

    Time: 52 ms (62.55%), Space: 42.4 MB (58.30%) - LeetHub

---
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    unordered_map<string, int> mp;
+    string
+    serializeTree(TreeNode *root, vector<TreeNode *> &nodes)
+    {
+        if (root == nullptr)
+            return "#";
+
+        string ser = to_string(root->val) + "," + serializeTree(root->left, nodes) + "," + serializeTree(root->right, nodes);
+        mp[ser] += 1;
+        if (mp[ser] ==2 ) // just push one element
+            nodes.push_back(root);
+
+        return ser;
+    }
+
+public:
+    vector<TreeNode *>
+    findDuplicateSubtrees(TreeNode *root)
+    {
+        vector<TreeNode *> nodes;
+
+        serializeTree(root, nodes);
+        return nodes;
+    }
+};
