e0b34ad93b7f7bc5351963e912697a5431cf68d3
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-11-29 18:42:01 +0200

    Time: 160 ms (56.67%), Space: 19.9 MB (62.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,28 +5,35 @@ public:
     {
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i <= nums.size() - 3; i++)
         {
-            if (i > 0 && nums[i - 1] == nums[i])
-                continue;
 
-            int left = i+1;
+            int left = i + 1;
             int right = nums.size() - 1;
             while (left < right)
             {
-                int sum =nums[i] + nums[left] + nums[right] ;
-                if (sum == 0)
+                int target = nums[i] + nums[left] + nums[right];
+                if (target == 0)
                 {
-                    res.push_back({{nums[i], nums[left], nums[right]}});
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    int templ=nums[left];
+                    int tempr=nums[right];
+                    // check if there are repeated values
+                    while (left < right && templ == nums[left + 1])
+                        left++;
+                    while (left < right && tempr == nums[right - 1])
+                        right--;
                     left++;
-                     while (left < right && nums[left]==nums[left-1]) left++;
-                }
-                else if (sum > 0)
                     right--;
-                else
+                }
+                else if (target < 0)
                     left++;
+                else
+                    right--;
             }
+        while (i + 1 <= nums.size()-3 && nums[i + 1] == nums[i]) 
+            i++;
+            
         }
         return res;
     }
