4a700dc20a478c94331335bdf30bfcc8db0e2aca
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-05 16:55:34 +0200

    Time: 0 ms (100.00%), Space: 6.7 MB (89.04%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -4,28 +4,25 @@ public:
     string addStrings(string num1, string num2)
     {
         int carry = 0;
-        int p1 = 0;
-        int p2 = 0;
+        int p1 = num1.size()-1;
+        int p2 = num2.size()-1;
         string result = "";
-        while (p1 < num1.size() || p2 < num2.size() || carry)
+        while (p1 >=0 || p2 >=0 || carry)
         {
 
             int sum=carry;
-            if (p1 < (int)num1.size())
-                sum+=num1[num1.size() - 1 - p1] - '0';
-            if (p2 < (int)num2.size())
-                sum+=num2[num2.size() - 1 - p2] - '0';
+            if (p1 >=0)
+                sum+=num1[p1] - '0';
+            if (p2 >=0)
+                sum+=num2[p2] - '0';
 
     
             carry = sum / 10;
             int resNum = sum % 10;
             result += '0' + resNum;
-            p1++;
-            p2++;
+            p1--;
+            p2--;
         }
-
-        if (carry)
-            result += '0' + carry;
 
         reverse(result.begin(), result.end());
         return result;
