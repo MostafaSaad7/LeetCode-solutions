530d18e07fe269bc643d910e27b2c33265589ca1
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-16 20:37:19 +0200

    Time: 6 ms (28.47%), Space: 6.1 MB (6.65%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,9 +4,10 @@ public:
     int hammingWeight(uint32_t n)
     {
         int ones = 0;
-        for (int i = 0; i < 32; i++, n=n >> 1)
+        for (;n;)
         {
-            ones +=n&1;
+            n =n&(n-1);
+            ones+=1;
         }
         return ones;
     }
