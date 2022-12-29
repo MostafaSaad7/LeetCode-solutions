eacc178dc5ac1895d488230799bb783e3373ab1f
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-29 11:08:54 +0200

    Time: 11 ms (50.55%), Space: 10.1 MB (83.94%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,24 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        int arr[2001]={0};
-        vector <int> v;
-        for(int i =0 ; i < nums1.size();i++)
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ans;
+        int i = 0;
+        int j = 0;
+        while (i < nums1.size() && j < nums2.size())
         {
-            arr[nums1[i]]+=1;
+            if (nums1[i] == nums2[j])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
         }
-          for(int i =0 ; i < nums2.size();i++)
-        {
-              if(arr[nums2[i]]>0)
-              {
-                  arr[nums2[i]]--;
-                  v.push_back(nums2[i]);
-              }
-           
-        }
-        
-        return v;
-        
-        
+
+        return ans;
     }
 };
