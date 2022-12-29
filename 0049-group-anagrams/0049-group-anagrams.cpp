4dac0df071ade935586d77bda662403087ebe20f
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-29 14:49:57 +0200

    Time: 34 ms (94.10%), Space: 19.6 MB (80.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,20 @@
-//dissc
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<vector<string>> res;
         unordered_map<string, vector<string>> mp;
-        for (string s : strs) {
-            string t = s; 
+        for (auto &s : strs)
+        {
+            string t = s;
             sort(t.begin(), t.end());
             mp[t].push_back(s);
         }
-        vector<vector<string>> anagrams;
-        for (auto p : mp) { 
-            anagrams.push_back(p.second);
+        for (auto &p : mp)
+        {
+            res.push_back(p.second);
         }
-        return anagrams;
+        return res;
     }
 };
