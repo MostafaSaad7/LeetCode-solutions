d5508f2eeb562ddb1fdf72f3ddf4410ea511becb
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-01 05:10:21 +0200

    Time: 50 ms (71.12%), Space: 17.8 MB (48.75%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+    ListNode *reverseList(ListNode *secondHalf)
+    {
+        ListNode *current = secondHalf;
+        ListNode *past = nullptr;
+        while (current)
+        {
+            ListNode *next = current->next;
+            current->next = past;
+            past = current;
+            current = next;
+        }
+        return past;
+    }
+    void reorderList(ListNode *head)
+    {
+        ListNode *middle = middleNode(head);
+        ListNode *tail = middle;
+        middle = middle->next;
+        tail->next = nullptr;
+        ListNode *secondHead = reverseList(middle);
+        ListNode *firstHead = head;
+        while (secondHead && firstHead)
+        {
+            ListNode *next1 = firstHead->next;
+            ListNode *next2 = secondHead->next;
+            firstHead->next = secondHead;
+            secondHead->next = next1;
+            firstHead = next1;
+            secondHead = next2;
+        }
+    }
+};
