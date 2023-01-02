c7c88375bf9d7ad07e3b1b6b725eca1f1cb8c926
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-02 21:35:46 +0200

    Time: 4 ms (46.69%), Space: 7.2 MB (36.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,10 +5,10 @@ public:
     {
         int left = 0, right = matrix.size() - 1;
         while (left < right)
-        {
+        {   
+            int top = left, bottom = right;
             for (int i = 0; i < (right - left); i++)
             {
-                int top = left, bottom = right;
                 int temp = matrix[top][left + i];
                 matrix[top][left + i] = matrix[bottom - i][left];
                 matrix[bottom - i][left] = matrix[bottom][right - i];
