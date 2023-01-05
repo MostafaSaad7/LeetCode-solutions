49eee91f4004d599c34eeca39380c6b9e6cad7c0
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-05 03:40:35 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (67.65%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,30 @@
+// Floyd's Cycle-Finding Algorithm >> Solutions
 class Solution
 {
+
+public:
+    int getNext(int n)
+    {
+        int totalSum = 0;
+        while (n > 0)
+        {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
 public:
     bool isHappy(int n)
     {
-        unordered_set<int> st;
-        while (!st.count(n))
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner)
         {
-            st.insert(n);
-            int newNum = 0;
-            while (n > 0)
-            {
-                newNum += pow((n % 10), 2);
-                n = n / 10;
-            }
-            n = newNum;
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
         }
-        return n == 1;
+        return fastRunner == 1;
     }
 };
