ece5de4f2e690d1b68c49d437b2ed1c3ed5df509
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-12 18:26:12 +0200

    Time: 5 ms (39.55%), Space: 6.5 MB (90.91%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLastWord(string s) {
         int spaceIndex = s.size()-1;
         int length= 0;
-        while(s[spaceIndex]==' ')
+        while(s[spaceIndex]==' ') // igonre ending spaces 
             spaceIndex--;
         while(spaceIndex>=0 && s[spaceIndex]!=' ')
         {
