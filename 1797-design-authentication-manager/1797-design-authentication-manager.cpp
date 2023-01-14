f65b9804852c63e7088be44d237a690a180d30b8
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-15 00:50:50 +0200

    Time: 164 ms (43.92%), Space: 30.3 MB (66.67%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.cpp
+++ 1797-design-authentication-manager/1797-design-authentication-manager.cpp
@@ -8,19 +8,18 @@ private:
 public:
     AuthenticationManager(int timeToLive) : timeToLive(timeToLive)
     {
-        
     }
 
     void generate(string tokenId, int currentTime)
     {
-        mp[tokenId] = currentTime;
+        mp[tokenId] = currentTime + timeToLive;
     }
 
     void renew(string tokenId, int currentTime)
     {
-        if (mp.count(tokenId) && (mp[tokenId] + timeToLive) > currentTime)
+        if (mp.count(tokenId) && mp[tokenId] > currentTime)
         {
-            mp[tokenId] = currentTime;
+            mp[tokenId] = currentTime + timeToLive;
         }
     }
 
@@ -29,7 +28,7 @@ public:
         int tokensNumber = 0;
         for (auto i : mp)
         {
-            if (i.second + timeToLive > currentTime)
+            if (i.second > currentTime)
                 tokensNumber++;
         }
         return tokensNumber;
