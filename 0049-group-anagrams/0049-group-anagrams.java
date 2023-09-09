c21a8c79ba39ce97301ac1c4aabaf369c29f78b6
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-09-09 12:45:06 +0300

    Time: 6 ms (80.83%), Space: 46.5 MB (88.12%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        
+        for (String str : strs) {
+            // Convert the string to a character array, sort it, and convert it back to a string.
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            // Check if the sorted string is already in the map, if not, create a new list for it.
+            if (!anagramGroups.containsKey(sortedStr)) {
+                anagramGroups.put(sortedStr, new ArrayList<>());
+            }
+            
+            // Add the original string to the corresponding group.
+            anagramGroups.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(anagramGroups.values());
+    }
+}
+
+
+
+
