54c1384be39c53173887b8388dbb334dcd3313c1
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-11-11 17:44:30 +0200

    Time: 1 ms (97.23%), Space: 40.3 MB (8.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,22 +2,24 @@
     The purpose is to find the greatest number where it's multiplication by itself id lesser than or equal x
  */
 class Solution {
-       double fun(double val)
-    {
-        return val * val;
-    }
     public int mySqrt(int x) {
-       double left = 0, right = x, mid = 0;
-        for (int i = 0; i < 100 ; i++)
-        {
-            mid = left + (right - left) / 2;
+        long left = 0;
+        long right = x ;
+        long result = left;
+        while (left <= right) {
+         long  middle = left + (right - left) / 2;
 
-            if (fun(mid) > x)
-                right = mid;
-            else
-                left = mid;
+            if ((middle * middle) == x) {
+                return (int)middle;
+            } else if ((middle * middle) > x) {
+                right = middle - 1;
+            } else {
+                result = middle;
+                left = middle + 1;
+            }
         }
 
-        return (int)(mid);
+
+        return (int)result;
     }
 }
