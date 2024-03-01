d4eff137ad700d4422aec079f4f67694c39d9244
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-01 19:07:36 +0300

    Time: 2 ms (83.47%), Space: 43.7 MB (37.94%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -3,19 +3,27 @@ class Solution {
 
         int length = s.length(); // Specify the desired length
         int ones = 0;
-        StringBuilder result = new StringBuilder();
-        result.append("0".repeat(length));
-        for (char c : s.toCharArray()) {
-            ones += c == '1' ? 1 : 0;
-        }
-        result.setCharAt(length-1, '1');
-        ones--;
-        int i = 0 ;
+        StringBuilder result = new StringBuilder(s);
+        int nextOnePlace = 0 ;
 
-        while (ones-->0)
+        for (int i = 0; i < s.length(); i++)
         {
-            result.setCharAt(i++,'1');
+            if (result.charAt(i)=='1')
+            {
+                result.setCharAt(i,result.charAt(nextOnePlace));
+                result.setCharAt(nextOnePlace++,'1');
+            }
+
+
+
         }
+
+        
+        char temp = result.charAt(length-1);
+        
+
+        result.setCharAt(length-1,result.charAt(nextOnePlace-1));
+        result.setCharAt(nextOnePlace-1,temp);
 
         return result.toString();
     }
