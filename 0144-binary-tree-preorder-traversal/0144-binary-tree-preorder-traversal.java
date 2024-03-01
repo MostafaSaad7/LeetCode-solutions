d1af2a6c30071b2e402800ed8cad8c4479ff0dd6
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-02 00:13:25 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (10.09%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,35 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-
-    public void preOrderTrav(TreeNode treeNode, List<Integer> treeNodeList) {
-        if (treeNode==null)
-            return;
-
-        treeNodeList.add(treeNode.val);
-        preOrderTrav(treeNode.left,treeNodeList);
-        preOrderTrav(treeNode.right,treeNodeList);
-
-    }
-
+    
+    List<Integer> res= new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
+        
+        dfs(root);
+        return res;
 
-        preOrderTrav(root,list);
-        return list;
     }
-
+    
+    void dfs(TreeNode root)
+    {
+        if (root==null)
+            return;
+        
+        res.add(root.val);
+        dfs(root.left);
+        dfs(root.right);
+    }
 }
