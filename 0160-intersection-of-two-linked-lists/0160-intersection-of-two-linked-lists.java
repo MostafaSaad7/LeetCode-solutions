e13b4d9bbbcfcb26ebe05d5b419cb1318a91cb22
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-11 21:35:50 +0300

    Time: 6 ms (26.63%), Space: 47.5 MB (95.94%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> hashSet = new HashSet<>();
+        while (headA!=null)
+        {
+            hashSet.add(headA);
+            headA=headA.next;
+        }
+
+        while (headB!=null)
+        {
+            if (hashSet.contains(headB)) return headB;
+
+            headB=headB.next;
+        }
+
+        return null;
+
+    }
+}
