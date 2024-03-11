fb54fac3e7605ec75a5e96e821dfa2df92c233af
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-11 21:29:14 +0300

    Time: 5 ms (76.66%), Space: 44 MB (52.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,10 +1,19 @@
 class Solution {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         if (x < 0)
             return false;
-        StringBuilder stringBuilder = new StringBuilder(String.valueOf(x));
+        long dividend = 1;
+        //x > dividend*10 the mean of this condition is do we need any other zero to be added to dividend
+        while (x >= dividend * 10) {
+            dividend *= 10;
+        }
+        while (x != 0) {
 
-        return stringBuilder.toString().equals(stringBuilder.reverse().toString());
-
-    }
-}
+            if (x % 10 != x / dividend)
+                return false;
+            x = (int) (x % dividend); // to remove left digit eg: 1221 % 1000 ---> 221
+            x = (x / 10); // to remove right digit
+            dividend /= 100; // because we removed 2 numbers 1 from the left and one from the rigt
+        }
+        return true;
+    }}
