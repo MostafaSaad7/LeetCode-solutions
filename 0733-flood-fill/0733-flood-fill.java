9c631fb35c814e357718d481f83338f70299faa0
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-12 01:37:36 +0300

    Time: 0 ms (100.00%), Space: 44.7 MB (80.12%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,30 @@
 class Solution {
-    boolean[][] visited;
+
+    int intialColor;
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        visited = new boolean[image.length][image[0].length]; 
+        intialColor = image[sr][sc];
         dfs(image, sr, sc, color);
         return image;
     }
 
+
     void dfs(int[][] image, int sr, int sc, int color) {
-        if (!isValidIndx(image, sr, sc) || visited[sr][sc]) return;
+        if (!isValidIndx(image, sr, sc) || image[sr][sc] != intialColor || image[sr][sc] == color) return;
 
-        visited[sr][sc] = true;
 
-        if (isValidIndx(image, sr + 1, sc) && image[sr][sc] == image[sr + 1][sc])
-            dfs(image, sr + 1, sc, color);
-        if (isValidIndx(image, sr - 1, sc) && image[sr][sc] == image[sr - 1][sc])
-            dfs(image, sr - 1, sc, color);
-        if (isValidIndx(image, sr, sc + 1) && image[sr][sc] == image[sr][sc + 1])
-            dfs(image, sr, sc + 1, color);
-        if (isValidIndx(image, sr, sc - 1) && image[sr][sc] == image[sr][sc - 1])
-            dfs(image, sr, sc - 1, color);
         image[sr][sc] = color;
+        dfs(image, sr + 1, sc, color);
+        dfs(image, sr - 1, sc, color);
+        dfs(image, sr, sc + 1, color);
+        dfs(image, sr, sc - 1, color);
+
     }
+
 
     boolean isValidIndx(int[][] image, int sr, int sc) {
         if (sr >= image.length || sr < 0 || sc >= image[0].length || sc < 0) return false;
         return true;
+
     }
 }
