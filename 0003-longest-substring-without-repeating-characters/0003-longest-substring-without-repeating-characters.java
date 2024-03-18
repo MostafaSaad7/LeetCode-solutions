d13d42d40464a02171fb960a800f54ddb6c9a67a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-19 01:57:46 +0300

    Time: 7 ms (43.79%), Space: 44.8 MB (27.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -8,27 +8,25 @@ class Solution {
             Output: 3
             Explanation: The answer is "wke", with the length of 3.
             Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-            
-            
+
+
             "dvdf"
     * */
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int right = 0;
         int res = 0;
         HashSet<Character> hashSet = new HashSet<>();
-        while (left < s.length() && right < s.length()) {
+        for (int right = 0, left = 0; right < s.length();) {
             if (!hashSet.contains(s.charAt(right))) {
                 hashSet.add(s.charAt(right++));
-                            res = Math.max(res, hashSet.size());
+                res = Math.max(res, hashSet.size());
             } else {
-                hashSet.remove(s.charAt(left));
-                left++;
-            }
+                    hashSet.remove(s.charAt(left));
+                    left++;
+                }
+
 
 
         }
-        res = Math.max(res, hashSet.size());
         return res;
     }
 }
