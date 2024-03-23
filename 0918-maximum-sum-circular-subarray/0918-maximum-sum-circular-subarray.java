dbad92b026122f6a7f9c1f890d583c500972edb4
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-23 18:42:03 +0300

    Time: 8 ms (18.37%), Space: 49.2 MB (33.59%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,36 +1,23 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
         if (nums.length == 1)
             return nums[0];
-        int maxSum = maxSum(nums);
-        int minSum = minSum(nums);
-        int fullArrSum = Arrays.stream(nums).sum();
-
-        if (fullArrSum==minSum)return maxSum; // -ve values for the whole array 
-        return Math.max(maxSum,fullArrSum-minSum);
-    }
-
-    int minSum(int[] nums) {
-        int sum = nums[0];
+        int sumMin = nums[0];
         int minSumValue = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sum = Math.min(nums[i], sum + nums[i]);
-            minSumValue = Math.min(sum, minSumValue);
-        }
-
-        return minSumValue;
-    }
-
-    int maxSum(int[] nums) {
-        int sum = nums[0];
+        int sumMax = nums[0];
         int maxSumValue = nums[0];
+        int fullArrSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(nums[i], sum + nums[i]);
-            maxSumValue = Math.max(sum, maxSumValue);
+            sumMin = Math.min(nums[i], sumMin + nums[i]);
+            minSumValue = Math.min(sumMin, minSumValue);
+
+            sumMax = Math.max(nums[i], sumMax + nums[i]);
+            maxSumValue = Math.max(sumMax, maxSumValue);
+
+            fullArrSum += nums[i];
         }
 
-        return maxSumValue;
+        if (fullArrSum == minSumValue) return maxSumValue; // -ve values for the whole array
+        return Math.max(maxSumValue, fullArrSum - minSumValue);
     }
 }
