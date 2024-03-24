4edd450ac28bc23cfc9b01e843ecab1063bfa17b
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-03-24 21:53:33 +0300

    Time: 3 ms (26.81%), Space: 46.1 MB (36.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        while (fast < nums.length) {
+            int slowNumber = nums[slow];
+            int fastNumber = nums[fast];
+            if (slowNumber != 0) {
+                slow = fast;
+            } else if (fastNumber != 0) {
+                nums[slow]=fastNumber;
+                nums[fast]=0;
+                slow++;
+            }
+
+            fast++;
+        }
+
+    }
+}
+/*
+    Input: nums = [1,3,12,13,16,0,0,12,23]
+                                s
+                                  f
+
+if not eq zero replace it with f
+
+s and f
+    1- s at non zero then s =f
+    2- f at zero and s at non zero so s = f
+    3- f at zero and s at zero then do nothing
+    4- f at non zero and s at zero then swap and increase s by 1
+
+    Output: [1,3,12,0,0]
+*
+*
+*
+* */
