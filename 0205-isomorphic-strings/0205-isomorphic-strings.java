3782fe9005e9cb9cfc510a7600a97a75440f38cf
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-04-02 03:43:03 +0300

    Time: 20 ms (15.77%), Space: 42.4 MB (37.68%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,18 +4,18 @@ import java.util.HashSet;
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) return false;
-        HashMap<Character, Character> characterHashMapS = new HashMap<>();
-        HashMap<Character, Character> characterHashMapT = new HashMap<>();
-
+        HashMap<Character, Character> characterHashMap = new HashMap<>();
+        HashSet<Character> mappedToItBefore = new HashSet<>();
 
         for (int i = 0; i < s.length(); i++) {
             char cS = s.charAt(i);
             char cT = t.charAt(i);
-            if ((characterHashMapS.containsKey(cS) && characterHashMapS.get(cS) != cT) || (characterHashMapT.containsKey(cT) && characterHashMapT.get(cT) != cS))
+            if (characterHashMap.containsKey(cS) && characterHashMap.get(cS) != cT)
                 return false;
-
-            characterHashMapS.put(cS, cT);
-            characterHashMapT.put(cT, cS);
+            if (!characterHashMap.containsKey(cS) && mappedToItBefore.contains(cT)) 
+                return false;
+            characterHashMap.put(cS, cT);
+            mappedToItBefore.add(cT);
         }
 
         return true;
