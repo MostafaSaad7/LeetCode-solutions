dde4322d0441c3532bcd694e11bcfd4d02ce3751
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-04 21:19:59 +0300

    Time: 50 ms (34.62%) | Memory: 43.9 MB (79.82%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = nums.length - 1; i > -1; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i])
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+
+            }
+
+
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18});
+    }
+}
