a3d61c81d3a6daf0054fd6a9ed0f5ea8ecf107c4
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-07 00:18:12 +0300

    Time: 2 ms (98.82%), Space: 45 MB (92.57%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int mx1 = Integer.MIN_VALUE, mx2 = Integer.MIN_VALUE, mx3 = Integer.MIN_VALUE;
+        int mn1 = Integer.MAX_VALUE, mn2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            if (num < mn1) {
+                mn2 = mn1;
+                mn1 = num;
+            } else if (num < mn2) {
+                mn2 = num;
+            }
+
+
+            if (num > mx1) {
+                mx3 = mx2;
+                mx2 = mx1;
+                mx1 = num;
+            } else if (num > mx2) {
+                mx3 = mx2;
+                mx2 = num;
+            } else if (num > mx3) {
+                mx3 = num;
+            }
+
+        }
+
+
+        return Math.max(mx1 * mx2 * mx3, mn1 * mn2 * mx1);
+
+
+    }
+}
