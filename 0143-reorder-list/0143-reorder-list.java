82516a036ee9829424b8e5ae72893691a04fd17d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-13 15:15:25 +0300

    Time: 2 ms (83.51%), Space: 47.9 MB (68.79%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,7 +10,7 @@ class Solution {
 
 
     private ListNode splitListIntoToHalfs(ListNode head) {
-        ListNode fast = head.next;
+        ListNode fast = head;
         ListNode slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
