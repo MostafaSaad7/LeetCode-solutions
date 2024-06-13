bffbd5138ebdcca11771a69a4a2c961f1d96e2d6
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-13 15:14:43 +0300

    Time: 2 ms (83.51%), Space: 48.9 MB (7.53%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void reorderList(ListNode head) {
+
+        ListNode secondHalfStart = splitListIntoToHalfs(head);
+        ListNode head2 = reverseLinkedList(secondHalfStart);
+        merge(head, head2);
+
+
+    }
+
+
+    private ListNode splitListIntoToHalfs(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode
+                secondHalfHead = slow.next;
+        slow.next = null;
+        return secondHalfHead;
+    }
+
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode curr1 = head1;
+        ListNode curr2 = head2;
+        while (curr1 != null && curr2 != null) {
+            ListNode next1 = curr1.next;
+            ListNode next2 = curr2.next;
+
+            curr1.next = curr2;
+            curr2.next = next1;
+
+            curr1 = next1;
+            curr2 = next2;
+        }
+
+        return head1;
+    }
+
+
+}
