6eb4f5a3ed748ab699e1258874298c69804d3790
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-13 03:16:22 +0300

    Time: 19 ms (88.4%), Space: 51 MB (26.32%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,43 +1,31 @@
 import java.util.Arrays;
 
 class Solution {
-    String text1RefCopy;
-    String text2RefCopy;
-
-    int[][] arrRef;
 
     public int longestCommonSubsequence(String text1, String text2) {
-        text1RefCopy = text1;
-        text2RefCopy = text2;
+
         int len1 = text1.length();
         int len2 = text2.length();
-        arrRef = new int[len1][len2];
-        
+        int[][] arrRef = new int[len1 + 1][len2 + 1];
+
         // Initialize the memoization array with -1
         for (int[] row : arrRef) {
-            Arrays.fill(row, -1);
+            Arrays.fill(row, 0);
         }
 
-        return getLongestCommonSubsequence(0, 0);
+        for (int i = len1 - 1; i >= 0; i--) {
+            for (int j = len2 - 1; j >= 0; j--) {
+
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    arrRef[i][j] = 1 + arrRef[i + 1][j + 1];
+                } else {
+
+                    arrRef[i][j] = Math.max(arrRef[i + 1][j], arrRef[i][j + 1]);
+                }
+            }
+        }
+        return arrRef[0][0];
     }
 
-    int getLongestCommonSubsequence(int idx1, int idx2) {
-        if (idx1 == text1RefCopy.length() || idx2 == text2RefCopy.length()) {
-            return 0;
-        }
 
-        if (arrRef[idx1][idx2] != -1) {
-            return arrRef[idx1][idx2];
-        }
-
-        if (text1RefCopy.charAt(idx1) == text2RefCopy.charAt(idx2)) {
-            arrRef[idx1][idx2] = 1 + getLongestCommonSubsequence(idx1 + 1, idx2 + 1);
-        } else {
-            int choice1 = getLongestCommonSubsequence(idx1 + 1, idx2);
-            int choice2 = getLongestCommonSubsequence(idx1, idx2 + 1);
-            arrRef[idx1][idx2] = Math.max(choice1, choice2);
-        }
-
-        return arrRef[idx1][idx2];
-    }
 }
