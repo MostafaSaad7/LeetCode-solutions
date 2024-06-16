d7220cf1941ec22dd1a321123d14499ce6b5c092
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-06-16 18:56:19 +0300

    Time: 0 ms (100%), Space: 40.6 MB (78.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,9 +3,6 @@ import java.util.Arrays;
 class Solution {
 
     int memory[] = new int[101];
-    private int currentHouse;
-    private int prevHouse;
-
     public int rob(int[] nums) {
         Arrays.fill(memory, -1);
         return robTheHouses(0, nums);
@@ -16,7 +13,7 @@ class Solution {
 
         if (currentHouse >= nums.length)
             return 0;
-        if (prevHouse != -1 && memory[currentHouse] != -1)
+        if (memory[currentHouse] != -1)
             return memory[currentHouse];
         int leaveThisHouseChoice = robTheHouses(currentHouse + 1, nums);
         int robThisHouseChoice = nums[currentHouse] + robTheHouses(currentHouse + 2, nums);
