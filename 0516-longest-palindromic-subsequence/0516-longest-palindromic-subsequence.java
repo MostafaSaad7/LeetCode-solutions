807c61f8e0297e030de4e29ce4b9aeffc63a9201
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-07-13 15:43:36 +0300

    Time: 24 ms (84.87%), Space: 57.9 MB (5.02%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return dfs(s, 0, s.length() - 1, new Integer[s.length()][s.length()]);
+    }
+
+
+    int dfs(String s, int start, int end, Integer[][] memory) {
+        if (start > end) {
+            return 0;
+        }
+        if (memory[start][end] != null) {
+            return memory[start][end];
+        }
+        int longestPalSubSeq = 0;
+        if (s.charAt(start) == s.charAt(end)) {
+            int length = start == end ? 1 : 2;
+            longestPalSubSeq = length + dfs(s, start + 1, end - 1, memory);
+        } else {
+            longestPalSubSeq = Math.max(dfs(s, start + 1, end, memory), dfs(s, start, end - 1, memory));
+        }
+        memory[start][end] = longestPalSubSeq;
+        return memory[start][end];
+    }
+}
