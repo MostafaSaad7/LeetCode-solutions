ce37b10bef22a8e106fa02d31a59e140cc98d659
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-07-28 22:16:55 +0300

    Time: 0 ms (100%), Space: 41.7 MB (31.28%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,18 +7,17 @@ class Solution {
         gridRows = obstacleGrid.length;
         gridCols = obstacleGrid[0].length;
         obstacleGridRef = obstacleGrid;
-        return uniquePaths(0, 0, new Integer[gridRows][gridCols]);
+        Integer[][] dp = new Integer[gridRows][gridCols];
+        dp[gridRows - 1][gridCols - 1] = 1;
+        return uniquePaths(0, 0, dp);
     }
 
     int uniquePaths(int row, int column, Integer[][] dp) {
         if (row == gridRows || column == gridCols || obstacleGridRef[row][column] == 1)
             return 0;
-        if (row == gridRows - 1 && column == gridCols - 1)
-            return 1;
-
         if (dp[row][column] != null)
             return dp[row][column];
-        
+
         int numberOfWays = uniquePaths(row + 1, column, dp) + uniquePaths(row, column + 1, dp);
         dp[row][column] = numberOfWays;
         return dp[row][column];
