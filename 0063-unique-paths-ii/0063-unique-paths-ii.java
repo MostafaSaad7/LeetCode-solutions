b0f036833b0cbd43dccfd2ffa9ac60feeb6b0f16
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-07-28 22:24:12 +0300

    Time: 1 ms (23.94%), Space: 41.3 MB (93.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,15 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     int gridRows;
     int gridCols;
     int[][] obstacleGridRef;
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        gridRows = obstacleGrid.length;
-        gridCols = obstacleGrid[0].length;
-        obstacleGridRef = obstacleGrid;
-        Integer[][] dp = new Integer[gridRows][gridCols];
-        dp[gridRows - 1][gridCols - 1] = 1;
-        return uniquePaths(0, 0, dp);
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+        Integer[] dp = new Integer[cols];
+        Arrays.fill(dp, 0);
+        dp[cols - 1] = 1;
+        for (int row = rows - 1; row > -1; row--) {
+            for (int col = cols - 1; col > -1; col--) {
+                if (obstacleGrid[row][col] == 1)
+                    dp[col] = 0;
+                else if (col + 1 < cols) {
+                    dp[col] = dp[col] + dp[col + 1]; // note that dp[col] on the right side calculated before from previous calculations
+                }
+
+            }
+
+        }
+
+        return dp[0];
     }
 
     int uniquePaths(int row, int column, Integer[][] dp) {
