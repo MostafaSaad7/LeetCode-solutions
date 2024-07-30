77f3595888843f7addfa094010fcd1e193f9c60d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-07-31 00:35:29 +0300

    Time: 9 ms (31.14%), Space: 56.2 MB (83.74%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -8,20 +8,23 @@ class Solution {
         cols = matrix[0].length;
         Integer[][] cache = new Integer[rows][cols];
 
-        int maxSide = 0;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                maxSide = Math.max(maxSide, dfs(r, c, cache));
-            }
+        // considered as topleft corner of the square
+        dfs(0, 0, cache);
+
+
+        int sideLen = 0;
+        for (Integer[] row : cache) {
+            for (int val : row)
+                sideLen = Math.max(sideLen, val);
         }
 
-        return maxSide * maxSide;
+        return sideLen * sideLen;
+
     }
 
     private int dfs(int row, int col, Integer[][] cache) {
+
         if (row == rows || col == cols)
-            return 0;
-        if (matrixRef[row][col] == '0')
             return 0;
         if (cache[row][col] != null)
             return cache[row][col];
@@ -30,18 +33,8 @@ class Solution {
         int down = dfs(row + 1, col, cache);
         int diagonal = dfs(row + 1, col + 1, cache);
 
-        int sideLen = 1 + Math.min(right, Math.min(down, diagonal));
+        int sideLen = matrixRef[row][col] == '1' ? 1 + Math.min(right, Math.min(down, diagonal)) : 0;
         cache[row][col] = sideLen;
         return sideLen;
     }
-
-    /*
-     * Time Complexity: O(N * M)
-     * - Each cell is visited once, and results are memoized.
-     * - There are N rows and M columns, so the total time complexity is O(N * M).
-     *
-     * Space Complexity: O(N * M)
-     * - The cache array stores results for each cell, requiring O(N * M) space.
-     * - The recursion stack can go as deep as O(N + M) in the worst case.
-     */
 }
