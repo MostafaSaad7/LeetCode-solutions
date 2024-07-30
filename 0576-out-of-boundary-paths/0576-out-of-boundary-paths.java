1651135433a594082d8b0dfff81f30ba67ba8190
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-07-30 22:58:42 +0300

    Time: 2 ms (100%), Space: 42.7 MB (27.52%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,40 +1,29 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
 
     private final int MOD = 1_000_000_007;
-    int rows, cols;
-    Map<String, Integer> cache;
+    private int rows, cols;
+    private Integer[][][] memo;
 
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         rows = m;
         cols = n;
-        cache = new HashMap<>();
+        memo = new Integer[m][n][maxMove + 1];
         return dfs(startRow, startColumn, maxMove);
     }
 
     private int dfs(int row, int column, int moves) {
-        if (moves == -1)
-            return 0;
-        if (row == -1 || row == rows || column == -1 || column == cols)
-            return 1;
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(row).append("#").append(column).append("#").append(moves);
-        String cacheKey = stringBuilder.toString();
-
-        if (cache.containsKey(cacheKey))
-            return cache.get(cacheKey);
+        if (moves < 0) return 0;
+        if (row < 0 || row >= rows || column < 0 || column >= cols) return 1;
+        
+        if (memo[row][column][moves] != null) return memo[row][column][moves];
 
         int paths = 0;
         paths = (paths + dfs(row + 1, column, moves - 1)) % MOD;
         paths = (paths + dfs(row - 1, column, moves - 1)) % MOD;
-        paths = (paths + dfs(row, column - 1, moves - 1)) % MOD;
         paths = (paths + dfs(row, column + 1, moves - 1)) % MOD;
-        
-        cache.put(cacheKey, paths);
+        paths = (paths + dfs(row, column - 1, moves - 1)) % MOD;
 
+        memo[row][column][moves] = paths;
         return paths;
     }
 }
