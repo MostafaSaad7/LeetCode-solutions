6e3557c80cbf1bf9e30928a6108682ec26e542b9
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-08-11 20:58:59 +0300

    Time: 4 ms (21.04%), Space: 46 MB (90.16%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> currentElements = new ArrayList<>();
+    Set<Integer> visited = new HashSet<>();
+    int[][] graph;
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        this.graph = graph;
+            dfs(0);
+ 
+        return result;
+    }
+
+    private boolean dfs(Integer node) {
+
+        visited.add(node);
+        currentElements.add(node);
+
+        if (node==graph.length-1)
+            result.add(new ArrayList<>(currentElements));
+        for (var neighbours : graph[node]) {
+            dfs(neighbours);
+        }
+
+        visited.remove(node);
+        currentElements.remove(node);
+
+
+//        for (var neighbours : graph[node]) {
+//            if (dfs(neighbours) && !visited.contains(neighbours)) {
+//                visited.add(neighbours);
+//                currentElements.add(node);
+//                result.add(currentElements);
+//                currentElements.remove(node);
+//                visited.remove(neighbours);
+//
+//            }
+//        }
+        return false;
+    }
+}
