f3faf03848f5f0d900fb4c908fa40eb75ed4e80a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-08-13 23:16:14 +0300

    Time: 2 ms (89.29%), Space: 45 MB (15.65%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,13 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Solution {
-    Set<Integer> positiveDiagonal = new HashSet<>(); //r+c
-    Set<Integer> negativeDiagonal = new HashSet<>(); // r-c
-    Set<Integer> colSet = new HashSet<>(); // column | col
-
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
         int[] queens = new int[n]; // Array to store the column position of queens, index represents the row
@@ -24,20 +18,21 @@ public class Solution {
         for (int col = 0; col < n; col++) {
             if (canPlaceQueen(row, col, queens)) {
                 queens[row] = col; // Place the queen
-                colSet.add(col);
-                positiveDiagonal.add(row + col);
-                negativeDiagonal.add(row - col);
                 backtrack(row + 1, queens, n, result); // Move to the next row
-                colSet.remove(col);
-                positiveDiagonal.remove(row + col);
-                negativeDiagonal.remove(row - col);
                 // Backtrack: After exploring the row, the queen is implicitly "removed" by overwriting in the next loop
             }
         }
     }
 
     private boolean canPlaceQueen(int row, int col, int[] queens) {
-        return !positiveDiagonal.contains(row + col) && !negativeDiagonal.contains(row - col) && !colSet.contains(col);
+        for (int i = 0; i < row; i++) {
+            int prevCol = queens[i];
+            // Check if the current column or any diagonals are occupied by a previously placed queen
+            if (prevCol == col || Math.abs(prevCol - col) == Math.abs(i - row)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private List<String> createBoard(int[] queens, int n) {
