311d04a6c519fa2dd5f0ac416b90e759ed3df997
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-09-28 14:31:45 +0300

    Time: 51 ms (34.24%), Space: 117.5 MB (26.62%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,26 +1,95 @@
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 class LRUCache {
 
-    private final Map<Integer, Integer> cache;
+    private class Node {
+
+        int key;
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(int key, int value, Node next, Node prev) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    int capacity;
+    Map<Integer, Node> cache;
+    Node left; // points to LRU node
+    Node right; // points to MRU node
 
     public LRUCache(int capacity) {
-        cache = new LinkedHashMap<>(capacity, 0.75f, true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.capacity = capacity;
+        cache = new HashMap<>();
+        left = new Node(0, 0);
+        right = new Node(0, 0);
+        
+        left.next = right;
+        right.prev = left;
     }
 
     public int get(int key) {
-        return cache.getOrDefault(key, -1);
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+
+        return -1;
     }
 
     public void put(int key, int value) {
-        cache.put(key, value);
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        cache.put(key, new Node(key, value));
+        insert(cache.get(key));
+
+        if (cache.size() > capacity) {
+            Node lru = left.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+
+
     }
+
+
+    // remove node form the list
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+
+    }
+
+
+    // insert node to the MRU ---> the right
+    private void insert(Node node) {
+
+        Node prev = right.prev;
+
+        node.prev = prev;
+        node.next = right;
+
+        prev.next = node;
+        right.prev = node;
+
+    }
+
 }
 
 /**
