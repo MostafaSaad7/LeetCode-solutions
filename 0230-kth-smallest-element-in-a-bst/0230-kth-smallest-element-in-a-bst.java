731bef0d2d49178b0e19106c95af3b996612689a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-09-30 21:17:07 +0300

    Time: 0 ms (100%), Space: 44.3 MB (63.05%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -20,28 +20,22 @@ class TreeNode {
 }
 
 class Solution {
-    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+      private static int number = 0;
+  private static int count = 0;
 
-    public int kthSmallest(TreeNode root, int k) {
-
-
-        dfs(root);
-        
-        int res =-1;
-        while (k--!=0)
-        {
-            res=priorityQueue.poll();
-        }
-        
-        return res;
-    }
-
-    private void dfs(TreeNode root) {
-        
-        if (root==null)
-            return;
-        priorityQueue.add(root.val);
-        dfs(root.left);
-        dfs(root.right);
-    }
+  public int kthSmallest(TreeNode root, int k) {
+      count = k;
+      helper(root);
+      return number;
+  }
+  
+  public void helper(TreeNode n) {
+      if (n.left != null) helper(n.left);
+      count--;
+      if (count == 0) {
+          number = n.val;
+          return;
+      }
+      if (n.right != null) helper(n.right);
+  }
 }
