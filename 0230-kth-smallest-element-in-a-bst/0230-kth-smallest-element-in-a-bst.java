b1d336d64649fb95f62395954f089fe05b06932d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-09-30 21:44:17 +0300

    Time: 0 ms (100%), Space: 44.6 MB (16.72%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,41 +1,24 @@
-import java.util.PriorityQueue;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
-      private static int number = 0;
-  private static int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>(); // Initialize stack for in-order traversal
 
-  public int kthSmallest(TreeNode root, int k) {
-      count = k;
-      helper(root);
-      return number;
-  }
-  
-  public void helper(TreeNode n) {
-      if (n.left != null) helper(n.left);
-      count--;
-      if (count == 0) {
-          number = n.val;
-          return;
-      }
-      if (n.right != null) helper(n.right);
-  }
+        while (root != null || !stack.isEmpty()) { // Continue until the root is null and the stack is empty
+
+            // Traverse to the leftmost node
+            while (root != null) {
+                stack.push(root); // Push current node to stack
+                root = root.left; // Move to the left child
+            }
+
+            TreeNode pop = stack.pop(); // Pop the node from the stack (smallest node)
+            k--; // Decrement k, since we've visited one more node
+            if (k == 0) // If k becomes 0, this is the k-th smallest node
+                return pop.val; // Return the value of the k-th smallest node
+
+            if (pop.right != null) // If the node has a right child
+                root = pop.right; // Move to the right child for further in-order traversal
+        }
+
+        return -1; // In case the tree has fewer than k nodes, return -1
+    }
 }
