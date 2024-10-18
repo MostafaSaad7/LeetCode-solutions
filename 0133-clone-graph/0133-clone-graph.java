00df0f33aff8f98c0d7a0ae6308f9e4f027e045e
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-10-18 13:03:10 +0300

    Time: 28 ms (20.72%), Space: 42.8 MB (39.21%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,18 +1,24 @@
+
+
+
 class Solution {
     Map<Integer, Node> visited = new HashMap<>();
 
     public Node cloneGraph(Node node) {
         if (node == null)
-            return null;
+            return node;
+
         if (visited.containsKey(node.val))
             return visited.get(node.val);
-        
+
         Node newNode = new Node(node.val);
         visited.put(node.val, newNode);
         
-        for (Node neighbor : node.neighbors) {
-            newNode.neighbors.add(cloneGraph(neighbor));
+        for (Node neighbour : node.neighbors) {
+
+            newNode.neighbors.add(cloneGraph(neighbour));
         }
+
         return newNode;
     }
 }
