647d99e342927129e877b165b15aaefe010966c5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-10-19 21:42:47 +0300

    Time: 0 ms (100%), Space: 40.9 MB (64.79%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -1,20 +1,20 @@
+// this one from techdose
 class Solution {
     public char findKthBit(int n, int k) {
         int inversionCount = 0;
         int len = (1 << n) - 1;
 
         while (k > 1) {
-            if (k == (len / 2) + 1) 
-                return (inversionCount & 1) == 1 ? '0' : '1';
+            if (k == len / 2 + 1)
+                return (inversionCount & 1) == 1 ? '0' : '1'; // Middle char is always '1'
 
+            // Find left half index if K is in 2nd half
             if (k > len / 2) {
                 k = len - k + 1;
                 inversionCount++;
-
             }
-            len = len / 2;
+            len /= 2;
         }
-
-        return (inversionCount & 1) == 1 ? '1' : '0';
+        return (inversionCount & 1) == 1 ? '1' : '0'; // First char is always '0'
     }
 }
