0ce67c37e28fc41b5e0b6ccc420745e7fec772b8
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-10-29 08:01:10 +0300

    Time: 7 ms (65.38%), Space: 56.8 MB (23.91%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -1,47 +1,34 @@
-import java.util.Arrays;
-
 class Solution {
-    private final int[] dirs = {-1, 0, 1};
-    
-    int dpSol(int[][] grid, int row, int col, int[][] dp) {
-
-        int M = grid.length;
-        int N = grid[0].length;
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-        
-        int maxMoves = 0;
-        for (int dir : dirs) {
-            int newRow = row + dir;
-            int newCol = col + 1;
-            
-            if (
-                    newRow >= 0 && newRow < M && newCol < N && grid[row][col] < grid[newRow][newCol]
-            ) {
-                maxMoves = Math.max(
-                        maxMoves,
-                        1 + dpSol(grid, newRow, newCol, dp)
-                );
-            }
-        }
-        dp[row][col] = maxMoves;
-        return maxMoves;
-    }
-
     public int maxMoves(int[][] grid) {
         int M = grid.length, N = grid[0].length;
-
         int[][] dp = new int[M][N];
+
         for (int i = 0; i < M; i++) {
-            Arrays.fill(dp[i], -1);
+            dp[i][0] = 1;
         }
-        
-        int maxMoves = 0;
-        for (int i = 0; i < M; i++) {
-            int movesRequired = dpSol(grid, i, 0, dp);
-            maxMoves = Math.max(maxMoves, movesRequired);
+
+        int maxCount = 0;
+        for (int j = 1; j < N; j++) {
+            for (int i = 0; i < M; i++) {
+
+                if (dp[i][j - 1] > 0 && grid[i][j] > grid[i][j - 1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j - 1] + 1);
+
+                }
+
+                if (i > 0 && dp[i - 1][j - 1] > 0 && grid[i][j] > grid[i - 1][j - 1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+
+                }
+
+                if (i < M - 1 && dp[i + 1][j - 1] > 0 && grid[i][j] > grid[i + 1][j - 1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i + 1][j - 1] + 1);
+                }
+
+                maxCount = Math.max(maxCount, dp[i][j] - 1);
+            }
         }
-        return maxMoves;
+
+        return maxCount;
     }
 }
