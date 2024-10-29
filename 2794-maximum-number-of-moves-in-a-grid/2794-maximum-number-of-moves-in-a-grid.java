2c8e8618990526679953634e78f85ab22ed2f2d3
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-10-29 07:33:30 +0300

    Time: 6 ms (80.77%), Space: 56.5 MB (42.75%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -1,54 +1,47 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 class Solution {
-
     private final int[] dirs = {-1, 0, 1};
+    
+    int dpSol(int[][] grid, int row, int col, int[][] dp) {
+
+        int M = grid.length;
+        int N = grid[0].length;
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        
+        int maxMoves = 0;
+        for (int dir : dirs) {
+            int newRow = row + dir;
+            int newCol = col + 1;
+            
+            if (
+                    newRow >= 0 && newRow < M && newCol < N && grid[row][col] < grid[newRow][newCol]
+            ) {
+                maxMoves = Math.max(
+                        maxMoves,
+                        1 + dpSol(grid, newRow, newCol, dp)
+                );
+            }
+        }
+        dp[row][col] = maxMoves;
+        return maxMoves;
+    }
 
     public int maxMoves(int[][] grid) {
         int M = grid.length, N = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] vis = new boolean[M][N];
-
-        // Enqueue the cells in the first column.
+        int[][] dp = new int[M][N];
         for (int i = 0; i < M; i++) {
-            vis[i][0] = true;
-            q.offer(new int[]{i, 0});
+            Arrays.fill(dp[i], -1);
         }
-
-        int levels = -1;
-        while (!q.isEmpty()) {
-            levels++;
-            int sz = q.size();
-            while (sz-- > 0) {
-                int[] v = q.poll();
-
-                // Current cell with the number of moves made so far.
-                int row = v[0], col = v[1];
-
-                for (int dir : dirs) {
-                    // Next cell after the move.
-                    int newRow = row + dir, newCol = col + 1;
-
-                    // If the next cell isn't visited yet and is greater than
-                    // the current cell value, add it to the queue with the
-                    // incremented move count.
-                    if (
-                            newRow >= 0 &&
-                                    newCol >= 0 &&
-                                    newRow < M &&
-                                    newCol < N &&
-                                    !vis[newRow][newCol] &&
-                                    grid[row][col] < grid[newRow][newCol]
-                    ) {
-                        vis[newRow][newCol] = true;
-                        q.offer(new int[]{newRow, newCol});
-                    }
-                }
-            }
+        
+        int maxMoves = 0;
+        for (int i = 0; i < M; i++) {
+            int movesRequired = dpSol(grid, i, 0, dp);
+            maxMoves = Math.max(maxMoves, movesRequired);
         }
-
-        return levels == -1 ? 0 : levels;
+        return maxMoves;
     }
 }
