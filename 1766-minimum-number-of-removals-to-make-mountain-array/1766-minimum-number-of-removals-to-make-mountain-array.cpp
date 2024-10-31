7abb056cc6ff0277e60856e32435e13a400241f1
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-10-31 23:10:36 +0300

    Time: 91 ms (11.27%), Space: 14.6 MB (97.59%) - LeetHub

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,60 @@
+vector<int> g;
+const int MAX = 1000 + 5;
+int memory[MAX][2];
+const int INCREASING = 0;
+const int DECREASING = 1;
+
+class Solution
+{
+    // longest increasing subsequence ends at index
+    int dpInc(int index)
+    {
+        if (index < 0)
+            return 0;
+        int &ret = memory[index][INCREASING];
+        if (ret != -1)
+            return ret;
+        ret = 0;
+        for (int i = index-1; i>=0; i--)
+        {
+            if (g[i] < g[index])
+                ret = max(ret, dpInc(i));
+        }
+        ret = ret + 1;
+        return ret;
+    }
+
+    // longest decreasing subsequence from current index
+    int dpDec(int index)
+    {
+        if (index == g.size())
+            return 0;
+        int &ret = memory[index][DECREASING];
+        if (ret != -1)
+            return ret;
+        ret = 0;
+        for (int i = index + 1; i < g.size(); i++)
+        {
+            if (g[i] < g[index])
+                ret = max(ret, dpDec(i));
+        }
+        ret = ret + 1;
+        return ret;
+    }
+
+public:
+    int minimumMountainRemovals(vector<int> &nums)
+    {
+        g = nums;
+        memset(memory, -1, sizeof(memory));
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (dpDec(i) == 1 || dpInc(i) == 1)
+                continue;
+            int temp = dpDec(i) + dpInc(i) - 1;
+            res = max(res, temp);
+        }
+        return nums.size() - res;
+    }
+};
