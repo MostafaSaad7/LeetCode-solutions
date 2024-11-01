dcdb39f212c8ca55120aab3718149d4dfd1a671c
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-11-02 01:53:13 +0200

    Time: 12 ms (30.75%), Space: 45.6 MB (53.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,53 +1,70 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Solution {
-    int[][] dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        boolean[][] visitedPacific = new boolean[heights.length][heights[0].length];
-        boolean[][] visitedAtlantic = new boolean[heights.length][heights[0].length];
-        List<List<Integer>> res = new ArrayList<>();
+        final int ROWS = heights.length;
+        final int COLS = heights[0].length;
 
-        for (int col = 0; col < heights[0].length; col++) {
-            dfs(heights, visitedPacific, -1, 0, col);
-            dfs(heights, visitedAtlantic, -1, heights.length - 1, col);
-        }
-        for (int row = 0; row < heights.length; row++) {
-            dfs(heights, visitedPacific, -1, row, 0);
-            dfs(heights, visitedAtlantic, -1, row, heights[0].length-1);
+        boolean[][] pacific = new boolean[ROWS][COLS];
+        boolean[][] atlantic = new boolean[ROWS][COLS];
+
+        Queue<int[]> pacificQueue = new LinkedList<>();
+        Queue<int[]> atlanticQueue = new LinkedList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < COLS; i++) {
+            pacificQueue.add(new int[]{0, i});
+            atlanticQueue.add(new int[]{ROWS - 1, i});
         }
 
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[0].length; j++) {
-                if (visitedPacific[i][j] && visitedAtlantic[i][j])
-                    res.add(List.of(i, j));
+        for (int i = 0; i < ROWS; i++) {
+            pacificQueue.add(new int[]{i, 0});
+            atlanticQueue.add(new int[]{i, COLS - 1});
+        }
+
+        bfs(pacificQueue, pacific, heights);
+        bfs(atlanticQueue, atlantic, heights);
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(List.of(i, j));
+                }
             }
         }
-        return res;
+
+        return result;
     }
 
-    void dfs(int[][] heights, boolean[][] visited, int prevNodeHeight, int row, int col) {
-        if (!isValid(heights, row, col) || visited[row][col] || prevNodeHeight > heights[row][col]) return;
+    private void bfs(Queue<int[]> queue, boolean[][] ocean, int[][] heights) {
+        while (!queue.isEmpty()) {
 
-        visited[row][col] = true;
+            int[] poll = queue.poll();
 
-        for (int[] dir : dirs) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-            if (!isValid(heights, newRow, newCol) || visited[newRow][newCol])
-                continue;
-            dfs(heights, visited, heights[row][col], newRow, newCol);
+            int row = poll[0];
+            int col = poll[1];
+
+            ocean[row][col] = true;
+
+            for (int[] dir : DIRS) {
+                int nRow = row + dir[0];
+                int nCol = col + dir[1];
+
+                if (valid(nRow, nCol, ocean) && !ocean[nRow][nCol] && heights[nRow][nCol] >= heights[row][col]) {
+                    queue.add(new int[]{nRow, nCol});
+                }
+
+            }
         }
-
-
     }
 
-
-    boolean isValid(int[][] heights, int row, int col) {
-        if (row >= heights.length || row < 0) {
-            return false;
-        } else return col >= 0 && col < heights[row].length;
+    private boolean valid(int nRow, int nCol, boolean[][] ocean) {
+        return nRow >= 0 && nRow < ocean.length && nCol >= 0 && nCol < ocean[0].length;
     }
 }
