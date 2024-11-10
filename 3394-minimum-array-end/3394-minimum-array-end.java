798edd93fa89207e5c3571e3f853256097c79189
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2024-11-10 02:43:13 +0200

    Time: 1750 ms (14.04%), Space: 40.6 MB (69.9%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -3,9 +3,7 @@ class Solution {
     public long minEnd(int n, int x) {
         long result = x;
 
-        // Step 1: Iterate n-1 times (since we already initialized result with x)
-        while (--n > 0) {
-            // Step 2: Increment result and perform bitwise OR with x
+        for (int i = 0; i < n - 1; i++) {
             result = (result + 1) | x;
         }
 
