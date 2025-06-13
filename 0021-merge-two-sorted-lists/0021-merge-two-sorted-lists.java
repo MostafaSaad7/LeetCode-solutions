77a4a166ca1ab1ec46ab7ce81cde88b40f4cbdf5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-06-13 19:06:53 +0300

    Time: 0 ms (100%), Space: 43 MB (15.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,44 +1,36 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list2 == null)
-            return list1;
-        else if (list1 == null)
-            return list2;
-        else {
-            ListNode newHead = new ListNode();
-            ListNode currentNode = newHead;
-            while (list1 != null && list2 != null) {
-                if (list1!=null && list1.val <= list2.val) {
-                    currentNode.next = list1;
-                    list1 = list1.next;
-                } else {
-                    currentNode.next = list2;
-                    list2 = list2.next;
-                }
-                currentNode=currentNode.next;
-            }
-            if (list1==null)
-            {
-                currentNode.next=list2;
-            }
-            else
-            {
-                currentNode.next=list1;
-            }
 
-            return newHead.next;
+class Solution {
+    /**
+     * Merges two sorted linked lists into a single sorted linked list.
+     *
+     * Time Complexity: O(n + m)
+     * - where n = number of nodes in list1, m = number of nodes in list2
+     * - each node is visited exactly once.
+     *
+     * Space Complexity: O(1)
+     * - constant extra space is used (no new nodes created, only pointers adjusted)
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to serve as the starting point
+        ListNode dummyHead = new ListNode(-1);
+        ListNode current = dummyHead;
+
+        // Traverse both lists and link the smaller node each time
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next; // move the current pointer forward
         }
 
-    }
+        // One of the lists may still have nodes left; link it directly
+        current.next = (list1 != null) ? list1 : list2;
 
+        // Return the head of the merged list (skip dummy node)
+        return dummyHead.next;
+    }
 }
