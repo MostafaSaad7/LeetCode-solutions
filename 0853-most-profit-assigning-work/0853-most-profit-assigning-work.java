4c922e5e51abe41d2ae5263c307ec3657f2a0854
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-06-23 09:01:48 +0300

    Time: 18 ms (81.2%), Space: 45.9 MB (48.52%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int n = difficulty.length;
+        int[][] jobs = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));  // sort by difficulty
+        Arrays.sort(worker);  // sort workers by ability
+
+        int maxProfit = 0, totalProfit = 0, i = 0;
+        for (int w : worker) {
+            while (i < n && jobs[i][0] <= w) {
+                maxProfit = Math.max(maxProfit, jobs[i][1]);
+                i++;
+            }
+            totalProfit += maxProfit;
+        }
+
+        return totalProfit;
+    }
+}
