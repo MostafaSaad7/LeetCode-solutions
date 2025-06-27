65de34c12ba877df93093f5e39deddc0316610a3
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-06-27 09:15:54 +0300

    Time: 33 ms (95.33%), Space: 56.4 MB (33.64%) - LeetHub

--- 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,19 +1,16 @@
 import java.util.Arrays;
-
 class Solution {
     public int reductionOperations(int[] nums) {
-        Arrays.sort(nums); // Sort the array
+        Arrays.sort(nums);
+        int n = nums.length;
         int operations = 0;
-        int count = 0;
-
-        // Iterate from the end to the start
-        for (int i = nums.length - 1; i > 0; i--) {
+        
+        // Process from largest to smallest (reverse iteration)
+        for (int i = n - 1; i > 0; i--) {
             if (nums[i] != nums[i - 1]) {
-                count++; // New unique value transition
+                operations += (n - i);
             }
-            operations += count;
         }
-
         return operations;
     }
 }
