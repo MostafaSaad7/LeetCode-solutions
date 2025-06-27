d5276a956c4877b997d4c06df9b7f0f1543d1eb4
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-06-27 09:21:14 +0300

    Time: 77 ms (14.02%), Space: 56.4 MB (33.64%) - LeetHub

--- 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,16 +1,46 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public int reductionOperations(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         int operations = 0;
         
-        // Process from largest to smallest (reverse iteration)
-        for (int i = n - 1; i > 0; i--) {
-            if (nums[i] != nums[i - 1]) {
-                operations += (n - i);
+        // Find all distinct values using binary search
+        for (int i = 0; i < n; ) {
+            int currentValue = nums[i];
+            
+            // Binary search to find the rightmost occurrence of currentValue
+            int rightmost = findRightmost(nums, currentValue);
+            
+            // All elements after rightmost position need to be reduced
+            // through this value level
+            int elementsToReduce = n - rightmost - 1;
+            operations += elementsToReduce;
+            
+            // Move to next distinct value
+            i = rightmost + 1;
+        }
+        
+        return operations;
+    }
+    
+    private int findRightmost(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                left = mid + 1;  // Continue searching right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return operations;
+        
+        return result;
     }
 }
