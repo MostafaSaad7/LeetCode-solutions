e933bffdbf1039cee13f4c0f45046872db168e11
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-07-12 23:50:20 +0300

    Time: 1 ms (85.97%), Space: 41.5 MB (6.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,28 @@
-
 class Solution {
     public int mySqrt(int x) {
-        long left = 0;
-        long right = x ;
-        long result = left;
-        while (left <= right) {
-         long  middle = left + (right - left) / 2;
-
-            if ((middle * middle) == x) {
-                return (int)middle;
-            } else if ((middle * middle) > x) {
-                right = middle - 1;
-            } else {
-                result = middle;
-                left = middle + 1;
-            }
+        if (x == 0) {
+            return 0;
         }
 
+        double left = 0;
+        double right = x;
+        double mid = 0; // Initialize mid outside the loop
 
-        return (int)result;
+        // Using a small epsilon for double comparison
+        // The loop continues as long as the difference between left and right is greater than EPSILON
+        final double EPSILON = 1e-9; 
+
+        while (Math.abs(left - right) > EPSILON) {
+            mid = left + (right - left) / 2;
+
+            if ((mid * mid) > x) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        
+
+        return (int) (mid + EPSILON); 
     }
 }
