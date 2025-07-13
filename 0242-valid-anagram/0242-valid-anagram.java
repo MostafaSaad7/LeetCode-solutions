7c03be28bd5065425f7f0f963317be510636db40
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-07-13 19:15:13 +0300

    Time: 22 ms (9.36%), Space: 44.5 MB (54.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,27 @@
+import java.util.HashMap;
+import java.util.Objects; // Although not strictly needed for this corrected logic, good to have if you intend to use it.
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length()!=t.length())
+        if (s.length() != t.length()) {
             return false;
-        int[] freq = new int[26];
-        for (char ch: s.toCharArray()) {
-            freq[ch-'a']++;
-        }
-        for (char ch: t.toCharArray()) {
-            freq[ch-'a']--;
         }
 
-        for (int chFreq: freq) {
-            if (chFreq!=0)
-                return false;
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            // For sMap
+            char sChar = s.charAt(i);
+            sMap.put(sChar, sMap.getOrDefault(sChar, 0) + 1);
+
+            // For tMap
+            char tChar = t.charAt(i);
+            tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
         }
 
-    return true;
+        // The most robust way to check if two frequency maps are equal is to use .equals()
+        // HashMap's equals method correctly compares key-value pairs.
+        return sMap.equals(tMap);
     }
-
 }
