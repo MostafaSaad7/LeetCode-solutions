b14e273c2b196390284f312ed2fa829ed02033eb
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-07-20 21:02:28 +0300

    Time: 1 ms (99.52%), Space: 56.8 MB (81.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,23 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        int sum = nums[0];
-        int res = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(nums[i] , nums[i] + sum);
-            res = Math.max(res, sum);
+        int sum=0;
+        int result = Integer.MIN_VALUE;
+        for (int end = 0; end < nums.length; end++) {
+            sum+=nums[end];
+            result=Math.max(result,sum);
+            if (sum<0)
+                sum=0;
 
         }
-
-        return res;
-
+return result;
     }
 }
-
-/*
-    Input: nums = [ 3, 1, -6,L R 4,-1,2,1,-5,4]
-    Output: 6
-    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-*
-*
-* */
