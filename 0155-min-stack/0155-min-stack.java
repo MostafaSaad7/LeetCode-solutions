cef43a193943a22d32f97f333b95927a04d2be22
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-07-23 20:15:03 +0300

    Time: 5 ms (38.44%), Space: 44.8 MB (68.7%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,30 +1,33 @@
-import java.util.AbstractMap;
-import java.util.Map;
 import java.util.Stack;
 
 class MinStack {
-    Stack<Map.Entry<Integer, Integer>> stack;
-    // Entry key is the inserted value into the stack, Entry value is the min value in the stack at this time
-
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
 
     public MinStack() {
         stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int val) {
-        int minValue = Math.min(stack.empty() ? val : stack.peek().getValue(), val);
-        stack.push(new AbstractMap.SimpleImmutableEntry<>(val,minValue));
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
     }
 
     public void pop() {
         stack.pop();
+        minStack.pop();
     }
 
     public int top() {
-        return stack.peek().getKey();
+        return stack.peek();
     }
 
     public int getMin() {
-        return stack.peek().getValue();
+        return minStack.peek();
     }
 }
