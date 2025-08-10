8bc73efd81ca8cf83f369e9b7ff31f525db23255
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-08-10 22:15:10 +0300

    Time: 20 ms (58.98%), Space: 100.6 MB (21.27%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
@@ -1,5 +1,48 @@
 class Solution {
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    boolean[][] visited;
+
     public boolean containsCycle(char[][] grid) {
-        
+        int m = grid.length, n = grid[0].length;
+        visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    if (dfs(grid, i, j, -1, -1, grid[i][j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] grid, int row, int col, int parentRow, int parentCol, char target) {
+        if (visited[row][col]) {
+            return true; // found a cycle
+        }
+
+        visited[row][col] = true;
+
+        for (int[] d : dirs) {
+            int newRow = row + d[0];
+            int newCol = col + d[1];
+
+            if (isValid(grid, newRow, newCol) &&
+                !(newRow == parentRow && newCol == parentCol) &&
+                grid[newRow][newCol] == target) {
+                
+                if (dfs(grid, newRow, newCol, row, col, target)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    boolean isValid(char[][] grid, int row, int col) {
+        return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
     }
 }
