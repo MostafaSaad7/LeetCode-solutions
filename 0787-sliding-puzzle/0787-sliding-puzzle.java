43fa1164dc9367b2e8536dc707663e50f6693617
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-08-25 12:29:26 +0300

    Time: 5 ms (95.14%), Space: 42.2 MB (94.94%) - LeetHub

--- 0787-sliding-puzzle/0787-sliding-puzzle.java
+++ 0787-sliding-puzzle/0787-sliding-puzzle.java
@@ -27,7 +27,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                sb.append(board[i][j]);  // Much cleaner than C++ version!
+                sb.append(board[i][j]); 
             }
         }
         String startState = sb.toString();
