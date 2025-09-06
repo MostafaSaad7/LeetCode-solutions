08606afac761d8b516c03cdc921845f077c1111c
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-09-06 16:27:21 +0300

    Time: 31 ms (38.17%), Space: 73.8 MB (33.51%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.java
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.java
@@ -2,37 +2,34 @@ import java.util.*;
 
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
+        // Build graph: course -> list of dependents
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int[] r : relations) {
-            int u = r[0], v = r[1];
+            int u = r[0] - 1; // prerequisite
+            int v = r[1] - 1; // dependent
             graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
         }
 
-        Map<Integer, Integer> memo = new HashMap<>();
+        int[] memo = new int[n]; // longest path from each course
+        int ans = 0;
 
-        // DFS function
-        class DFS {
-            int run(int course) {
-                if (memo.containsKey(course)) return memo.get(course);
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfsForward(i, graph, time, memo));
+        }
+        return ans;
+    }
 
-                int res = time[course - 1]; // base: if no dependents
-                if (graph.containsKey(course)) {
-                    for (int nei : graph.get(course)) {
-                        res = Math.max(res, time[course - 1] + run(nei));
-                    }
-                }
+    private int dfsForward(int course, Map<Integer, List<Integer>> graph, int[] time, int[] memo) {
+        if (memo[course] > 0) return memo[course];
 
-                memo.put(course, res);
-                return res;
+        int res = time[course];
+        if (graph.containsKey(course)) {
+            for (int nei : graph.get(course)) {
+                res = Math.max(res, time[course] + dfsForward(nei, graph, time, memo));
             }
         }
 
-        DFS dfs = new DFS();
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            ans = Math.max(ans, dfs.run(i));
-        }
-
-        return ans;
+        memo[course] = res;
+        return res;
     }
 }
