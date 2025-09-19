53147d21bf3c603b85f5fdc9cf7e2a23600c5613
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-09-19 22:06:53 +0300

    Time: 21 ms (52.17%), Space: 51.4 MB (9.58%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,31 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
-
     public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-        int len1 = text1.length();
-        int len2 = text2.length();
-        int[][] arrRef = new int[len1 + 1][len2 + 1];
-
-        // Initialize the memoization array with -1
-        for (int[] row : arrRef) {
-            Arrays.fill(row, 0);
-        }
-
-        for (int i = len1 - 1; i >= 0; i--) {
-            for (int j = len2 - 1; j >= 0; j--) {
-
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    arrRef[i][j] = 1 + arrRef[i + 1][j + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-
-                    arrRef[i][j] = Math.max(arrRef[i + 1][j], arrRef[i][j + 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        return arrRef[0][0];
+
+        return dp[m][n];
+
     }
-
-
 }
