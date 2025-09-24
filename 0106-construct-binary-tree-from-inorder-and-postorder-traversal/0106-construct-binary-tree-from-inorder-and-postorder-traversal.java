62444b8b54ab2f4a2da89e246d48391d6878549d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-09-24 21:20:41 +0300

    Time: 2 ms (57.54%), Space: 44.6 MB (29.85%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,10 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
@@ -13,28 +21,36 @@ public class TreeNode {
 }
 
 class Solution {
-    private int postIdx;  // \U0001f448 global index
+    private int postIdx;
+    private Map<Integer, Integer> inorderIndexMap;
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        postIdx = postorder.length - 1; // start from the last element
+        postIdx = postorder.length - 1;
+
+        // Map value -> index in inorder for O(1) lookups
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
+        }
+
         return BuildBinaryTree(inorder, postorder, 0, inorder.length - 1);
     }
 
-    TreeNode BuildBinaryTree(int[] inorder, int[] postorder, int inOrderLeft, int inOrderRight) {
-        if (postIdx < 0 || inOrderLeft > inOrderRight)
+    TreeNode BuildBinaryTree(int[] inorder, int[] postorder, int inLeft, int inRight) {
+        if (inLeft > inRight)
             return null;
 
-        for (int i = inOrderLeft; i <= inOrderRight; i++) {
-            if (inorder[i] == postorder[postIdx]) {
-                TreeNode root = new TreeNode(postorder[postIdx--]);  
+        // Root is always the current postIdx
+        int rootVal = postorder[postIdx--];
+        TreeNode root = new TreeNode(rootVal);
 
-                // build right first
-                root.right = BuildBinaryTree(inorder, postorder, i + 1, inOrderRight);
-                root.left = BuildBinaryTree(inorder, postorder, inOrderLeft, i - 1);
-                return root;
-            }
-        }
+        // Split inorder into left/right subtrees
+        int inorderIndex = inorderIndexMap.get(rootVal);
 
-        return null;
+        // Build right subtree first (because postorder goes left->right->root)
+        root.right = BuildBinaryTree(inorder, postorder, inorderIndex + 1, inRight);
+        root.left = BuildBinaryTree(inorder, postorder, inLeft, inorderIndex - 1);
+
+        return root;
     }
 }
