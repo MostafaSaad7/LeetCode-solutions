7fea240f8be2762be4646f63b643b82e64e0ea41
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2025-11-09 21:08:24 +0200

    Time: 31 ms (10.94%), Space: 57.2 MB (6.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,13 +1,34 @@
-class Solution {
-    public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1; // Base case: there's one way to make 0 amount (use no coins)
+import java.util.Arrays;
 
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] += dp[j - coin];
-            }
+class Solution {
+    int[][] dp;
+
+    public int change(int amount, int[] coins) {
+        dp = new int[coins.length][amount + 1];
+        for (var arr : dp) {
+            Arrays.fill(arr, -1);
+
         }
-        return dp[amount];
+        return solve(0, coins, amount);
+
+    }
+
+
+    int solve(int idx, int[] coins, int amount) {
+        if (amount == 0)
+            return 1;
+        if (amount < 0 || idx >= coins.length)
+            return 0;
+
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
+
+
+        int c1 = solve(idx, coins, amount - coins[idx]);
+        int c2 = solve(idx + 1, coins, amount);
+
+        dp[idx][amount] = c1 + c2;
+
+        return dp[idx][amount];
     }
 }
