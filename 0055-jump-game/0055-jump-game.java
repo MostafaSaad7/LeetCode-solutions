dc698a1528f4ca93462fa26d1559e48341134f41
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2026-02-02 22:52:17 +0200

    Time: 3 ms (37.26%), Space: 48 MB (56.56%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal=nums.length-1;
-        for (int pos = nums.length-1; pos >=0 ; pos--) {
-            if (pos+nums[pos]>=goal)
-                goal=pos;
+        int maxJump =0;
+        for (int pos =0 ; pos < nums.length ; pos++) {
+            if(pos > maxJump) return false;
+            maxJump=Math.max(maxJump, nums[pos]+pos);
         }
 
-        return goal==0;
+        return true;
     }
 
 }
