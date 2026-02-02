550d3d5cade2899d7051957f45408dc32ac66bb4
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2026-02-02 22:49:59 +0200

    Time: 1 ms (99.8%), Space: 48 MB (36.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,27 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        return dp(nums, 0, new Boolean[nums.length]);
-    }
-    
-    private boolean dp(int[] nums, int index, Boolean[] memo) {
-        if (index == nums.length - 1)
-            return true;
-        if (index >= nums.length)
-            return false;
-        if (memo[index] != null)
-            return memo[index];
-        
-        boolean result = false;
-     
-        for (int jump = 1; jump <= nums[index]; jump++) {
-            int newIndex = index + jump;
-            if (newIndex < nums.length) {
-                result |= dp(nums, newIndex, memo);
-                if (result) break; 
-            }
+        int goal=nums.length-1;
+        for (int pos = nums.length-1; pos >=0 ; pos--) {
+            if (pos+nums[pos]>=goal)
+                goal=pos;
         }
-        
-        memo[index] = result;
-        return result;
+
+        return goal==0;
     }
+
 }
