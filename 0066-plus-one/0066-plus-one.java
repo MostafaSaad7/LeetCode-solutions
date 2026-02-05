5b43f6509fec84a9e97db58d8331607821548b48
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2026-02-05 22:48:59 +0200

    Time: 0 ms (100%), Space: 43.7 MB (17.76%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 1;
-
-        for(int i = digits.length-1 ; i >=0;i--)
-        {
-            int  tempResult = digits[i]+carry;
-            digits[i]=tempResult%10;
-            carry=tempResult/10;
-            if(carry==0)
-            return digits;
+        int carry = 1;  
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int temp = carry + digits[i];
+            result.add(temp % 10);  
+            carry = temp / 10;       
         }
-        if(carry!=0)
-        {
-            int tempArr[] =new int[digits.length+1];
-            int i =tempArr.length-1;
-            for( ; i >0;i--)
-            {
-                tempArr[i]=digits[i-1];
-            }
-            tempArr[i]=carry;
-            return tempArr;
+        
+        if (carry > 0) {
+            result.add(carry);
         }
-
-        return digits;
+        
+        
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(result.size() - 1 - i);
+        }
+        
+        return answer;
     }
 }
